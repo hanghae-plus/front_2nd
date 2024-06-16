5a83ac8a9f60fd13b86338e899479f50bde6d900
ChangigJeon <wjs1724@naver.com>
2024-06-16 23:29:28 +0900

    feat 24.06.16 1주차 과제 solve

--- packages/assignment-1/src/advanced/useMyRef.ts
+++ packages/assignment-1/src/advanced/useMyRef.ts
@@ -1,3 +1,10 @@
-export function useMyRef<T>(initValue: T | null) {
-  return { current: initValue }
+import { useState } from 'react'
+
+interface RefObject<T> {
+  current : T | null
+}
+
+export function useMyRef<T>(initValue: T | null):RefObject<T> {
+  const [ref] = useState<RefObject<T>>({ current: initValue });
+  return ref;
 }
