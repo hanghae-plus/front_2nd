e81d420e1de2e13ae6f78cc255ac77e527548946
AnSuebin <jejupalette@naver.com>
2024-06-17 08:38:10 +0900

    feat : useCallback 기능 수정

--- packages/assignment-1/src/basic/UseCallbackTest.tsx
+++ packages/assignment-1/src/basic/UseCallbackTest.tsx
@@ -1,16 +1,23 @@
-import { useState } from "react";
+import React, { useCallback, useState } from "react";
 import { BarkButton, MeowButton } from "./UseCallbackTest.components.tsx";
 
-export default function UseCallbackTest() {
+export default React.memo(function UseCallbackTest() {
   const [meowCount, setMeowCount] = useState(0);
   const [barkedCount, setBarkedCount] = useState(0);
+
+  const increaseMeowCount = useCallback(() => {
+    setMeowCount((n) => n + 1);
+  }, []);
+  const increaseBarkCount = useCallback(() => {
+    setBarkedCount((n) => n + 1);
+  }, []);
 
   return (
     <div>
       <p data-testid="cat">meowCount {meowCount}</p>
       <p data-testid="dog">barkedCount {barkedCount}</p>
-      <MeowButton onClick={() => setMeowCount(n => n + 1)}/>
-      <BarkButton onClick={() => setBarkedCount(n => n + 1)}/>
+      <MeowButton onClick={increaseMeowCount} />
+      <BarkButton onClick={increaseBarkCount} />
     </div>
   );
-}
+});
