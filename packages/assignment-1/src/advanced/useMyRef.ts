fbe9ca0af0ab0f6c6d70169a3deb6d127e73baa6
mosozeep <108648919+mosohyun@users.noreply.github.com>
2024-06-17 00:00:50 +0900

    useMyRef 수정 중

--- packages/assignment-1/src/advanced/useMyRef.ts
+++ packages/assignment-1/src/advanced/useMyRef.ts
@@ -1,3 +1,13 @@
+let refArr: any = [];
+let index = 0;
+
 export function useMyRef<T>(initValue: T | null) {
-  return { current: initValue }
+  const localIndex = index;
+  refArr.push({ current: initValue });
+  // index++;
+  return refArr[localIndex];
 }
+
+// export function useMyRef<T>(initValue: T | null) {
+//   return { current: initValue };
+// }
