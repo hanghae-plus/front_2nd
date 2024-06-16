42a669d9afbd69086d0a2e6e1c064a539e0e18db
mosozeep <108648919+mosohyun@users.noreply.github.com>
2024-06-16 19:09:19 +0900

    step1 완료

--- packages/assignment-1/src/basic/PureComponentTest.tsx
+++ packages/assignment-1/src/basic/PureComponentTest.tsx
@@ -1,16 +1,26 @@
-import { useState } from 'react';
+import { useState, PureComponent } from 'react';
 import { Cat, Dog } from './PureComponentTest.components.tsx';
 
 // NOTE: 다른 파일은 수정하지 않고, 현재 파일만 수정하여 문제를 해결해주세요.
+class PureCat extends PureComponent<any, any> {
+  render() {
+    return <Cat crying={this.props.crying} />;
+  }
+}
+class PureDog extends PureComponent<any, any> {
+  render() {
+    return <Dog crying={this.props.crying} />;
+  }
+}
+
 export default function PureComponentTest() {
   const [meowCount, setMeowCount] = useState(1);
   const [barkedCount, setBarkedCount] = useState(1);
 
-  console.log(meowCount, barkedCount);
   return (
     <div>
-      <Cat crying={meowCount} />
-      <Dog crying={barkedCount} />
+      <PureCat crying={meowCount} />
+      <PureDog crying={barkedCount} />
       <button data-testid='meow' onClick={() => setMeowCount((n) => n + 1)}>
         야옹
       </button>
--- packages/assignment-1/src/basic/UseCallbackTest.tsx
+++ packages/assignment-1/src/basic/UseCallbackTest.tsx
@@ -1,16 +1,33 @@
-import { useState } from "react";
-import { BarkButton, MeowButton } from "./UseCallbackTest.components.tsx";
+import { useState, PureComponent, useCallback } from 'react';
+import { BarkButton, MeowButton } from './UseCallbackTest.components.tsx';
+
+class PureComp extends PureComponent<any, any> {
+  render() {
+    return (
+      <p data-testid={this.props.id}>
+        {this.props.text} {this.props.count}
+      </p>
+    );
+  }
+}
 
 export default function UseCallbackTest() {
   const [meowCount, setMeowCount] = useState(0);
   const [barkedCount, setBarkedCount] = useState(0);
 
+  const onMeowClick = useCallback(() => {
+    setMeowCount((n) => n + 1);
+  }, []);
+  const onBarkClick = useCallback(() => {
+    setBarkedCount((n) => n + 1);
+  }, []);
+
   return (
     <div>
-      <p data-testid="cat">meowCount {meowCount}</p>
-      <p data-testid="dog">barkedCount {barkedCount}</p>
-      <MeowButton onClick={() => setMeowCount(n => n + 1)}/>
-      <BarkButton onClick={() => setBarkedCount(n => n + 1)}/>
+      <PureComp id='cat' text='meowCount' count={meowCount} />
+      <PureComp id='dog' text='barkedCount' count={barkedCount} />
+      <MeowButton onClick={onMeowClick} />
+      <BarkButton onClick={onBarkClick} />
     </div>
   );
 }
