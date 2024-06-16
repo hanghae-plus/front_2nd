87837b64e11fe8f6d0901ca9d7073b1414ed8dd4
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2024-06-16 17:48:14 +0900

    feat : useMemoTest

--- packages/assignment-1/src/basic/UseStateTest.tsx
+++ packages/assignment-1/src/basic/UseStateTest.tsx
@@ -1,14 +1,26 @@
 import { useState } from "react";
 
-
 // NOTE: state의 값이 정상적으로 변경이 되도록 만들어주세요.
 export default function UseStateTest() {
   const [state, setState] = useState({ bar: { count: 1 } });
 
+  // const increment = () => {
+  //   state.bar.count += 1;
+  //   setState(state);
+  // }
+
   const increment = () => {
-    state.bar.count += 1;
-    setState(state);
-  }
+    setState((prev) => {
+      /**Object 복사 */
+      const newValue = {
+        ...prev,
+      };
+
+      newValue.bar.count = prev.bar.count + 1;
+
+      return newValue;
+    });
+  };
 
   return (
     <div>
