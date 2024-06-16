ca080125a5a0857707f2b047605cb269eafcc830
AnSuebin <jejupalette@naver.com>
2024-06-17 08:56:50 +0900

    feat : RequireRefactoring 기능 수정

--- packages/assignment-1/src/basic/RequireRefactoring.tsx
+++ packages/assignment-1/src/basic/RequireRefactoring.tsx
@@ -2,22 +2,35 @@ import { ComponentProps, memo, PropsWithChildren } from "react";
 
 type Props = {
   countRendering?: () => void;
-}
+};
 
-const PureComponent = memo(({ children, countRendering, ...props }: PropsWithChildren<ComponentProps<'div'> & Props>) => {
-  countRendering?.();
-  return <div {...props}>{children}</div>
-})
+const PureComponent = memo(
+  ({
+    children,
+    countRendering,
+    ...props
+  }: PropsWithChildren<ComponentProps<"div"> & Props>) => {
+    countRendering?.();
+    return <div {...props}>{children}</div>;
+  }
+);
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-let outerCount = 1
+let outerCount = 1;
+
+// PureComponent에 전달되는 props가 불필요하게 변경되지 않도록 해주는 것
+const style = { width: "100px", height: "100px" };
+
+const handleClick = () => {
+  outerCount += 1;
+};
 
 // useMemo, useCallback 등을 사용하지 않고 이 컴포넌트를 개선해보세요.
 export default function RequireRefactoring({ countRendering }: Props) {
   return (
     <PureComponent
-      style={{ width: '100px', height: '100px' }}
-      onClick={() => {outerCount += 1;}}
+      style={style}
+      onClick={handleClick}
       countRendering={countRendering}
     >
       test component
