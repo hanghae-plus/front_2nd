e06e49558f237bf295e0bfcf606bcb983da8183f
metacode22 <alohajune22@gmail.com>
2024-06-16 18:16:36 +0900

    feat: useRef를 이용해 불필요한 렌더링 제거

--- packages/assignment-1/src/basic/RequireRefactoring.tsx
+++ packages/assignment-1/src/basic/RequireRefactoring.tsx
@@ -1,23 +1,34 @@
-import { ComponentProps, memo, PropsWithChildren } from "react";
+import { ComponentProps, memo, PropsWithChildren, useRef } from 'react';
 
 type Props = {
   countRendering?: () => void;
-}
+};
 
-const PureComponent = memo(({ children, countRendering, ...props }: PropsWithChildren<ComponentProps<'div'> & Props>) => {
-  countRendering?.();
-  return <div {...props}>{children}</div>
-})
+const PureComponent = memo(
+  ({
+    children,
+    countRendering,
+    ...props
+  }: PropsWithChildren<ComponentProps<'div'> & Props>) => {
+    countRendering?.();
+    return <div {...props}>{children}</div>;
+  }
+);
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-let outerCount = 1
+let outerCount = 1;
 
 // useMemo, useCallback 등을 사용하지 않고 이 컴포넌트를 개선해보세요.
 export default function RequireRefactoring({ countRendering }: Props) {
+  const styleRef = useRef({ width: '100px', height: '100px' });
+  const onClickRef = useRef(() => {
+    outerCount += 1;
+  });
+
   return (
     <PureComponent
-      style={{ width: '100px', height: '100px' }}
-      onClick={() => {outerCount += 1;}}
+      style={styleRef.current}
+      onClick={onClickRef.current}
       countRendering={countRendering}
     >
       test component
