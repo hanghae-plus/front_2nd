c5bd4f26630abac3e0777ea2dc04b3776e6aa5e3
alicelee1231 <saemmilee1231@gmail.com>
2024-06-18 23:23:45 +0900

    first assignment

--- packages/assignment-1/src/basic/PureComponentTest.tsx
+++ packages/assignment-1/src/basic/PureComponentTest.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Cat, Dog } from "./PureComponentTest.components.tsx";
 
-
 // NOTE: 다른 파일은 수정하지 않고, 현재 파일만 수정하여 문제를 해결해주세요.
 export default function PureComponentTest() {
   const [meowCount, setMeowCount] = useState(1);
@@ -9,10 +8,14 @@ export default function PureComponentTest() {
 
   return (
     <div>
-      <Cat crying={meowCount}/>
-      <Dog crying={barkedCount}/>
-      <button data-testid="meow" onClick={() => setMeowCount(n => n + 1)}>야옹</button>
-      <button data-testid="bark" onClick={() => setBarkedCount(n => n + 1)}>멍멍</button>
+      <Cat crying={meowCount} />
+      <Dog crying={barkedCount} />
+      <button data-testid="meow" onClick={() => setMeowCount((n) => n + 1)}>
+        야옹
+      </button>
+      <button data-testid="bark" onClick={() => setBarkedCount((n) => n + 1)}>
+        멍멍
+      </button>
     </div>
   );
 }
--- packages/assignment-1/src/basic/UseStateTest.tsx
+++ packages/assignment-1/src/basic/UseStateTest.tsx
@@ -1,18 +1,16 @@
 import { useState } from "react";
 
-
 // NOTE: state의 값이 정상적으로 변경이 되도록 만들어주세요.
 export default function UseStateTest() {
-  const [state, setState] = useState({ bar: { count: 1 } });
+  const [state, setState] = useState(1);
 
   const increment = () => {
-    state.bar.count += 1;
-    setState(state);
-  }
+    setState(state + 1);
+  };
 
   return (
     <div>
-      count: {state.bar.count}
+      count: {state}
       <button onClick={increment}>증가</button>
     </div>
   );
