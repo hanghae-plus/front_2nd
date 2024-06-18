b11e3014986a43f3c227c15e81c3c6d0ca13899d
Rachel <borapark13@gmail.com>
2024-06-18 22:54:03 +0900

    fix: props로 컴포넌트의 렌더링 최적화

--- packages/assignment-1/src/basic/RequireRefactoring.tsx
+++ packages/assignment-1/src/basic/RequireRefactoring.tsx
@@ -1,24 +1,41 @@
-import { ComponentProps, memo, PropsWithChildren } from "react";
+import { ComponentProps, memo, PropsWithChildren, useState } from "react";
 
 type Props = {
   countRendering?: () => void;
-}
+};
 
-const PureComponent = memo(({ children, countRendering, ...props }: PropsWithChildren<ComponentProps<'div'> & Props>) => {
-  countRendering?.();
-  return <div {...props}>{children}</div>
-})
+const PureComponent = memo(
+  ({
+    children,
+    countRendering,
+    ...props
+  }: PropsWithChildren<ComponentProps<"div"> & Props>) => {
+    countRendering?.();
+    return <div {...props}>{children}</div>;
+  }
+);
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-let outerCount = 1
+let outerCount = 1;
 
 // useMemo, useCallback 등을 사용하지 않고 이 컴포넌트를 개선해보세요.
 export default function RequireRefactoring({ countRendering }: Props) {
+  const [counterRenderingState] = useState(countRendering);
+
+  const handleCountRendering = () => {
+    if (counterRenderingState !== countRendering) {
+      return counterRenderingState;
+    }
+    return countRendering;
+  };
+
   return (
     <PureComponent
-      style={{ width: '100px', height: '100px' }}
-      onClick={() => {outerCount += 1;}}
-      countRendering={countRendering}
+      style={{ width: "100px", height: "100px" }}
+      onClick={() => {
+        outerCount += 1;
+      }}
+      countRendering={handleCountRendering}
     >
       test component
     </PureComponent>
