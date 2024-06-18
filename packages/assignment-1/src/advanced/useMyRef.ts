5f9ccaa9e726738c2b5b9dddbaddbc57338c18ba
mosozeep <108648919+mosohyun@users.noreply.github.com>
2024-06-18 22:58:58 +0900

    useMyRef 완료

--- packages/assignment-1/src/advanced/useMyRef.ts
+++ packages/assignment-1/src/advanced/useMyRef.ts
@@ -2,12 +2,31 @@ let refArr: any = [];
 let index = 0;
 
 export function useMyRef<T>(initValue: T | null) {
-  const localIndex = index;
-  refArr.push({ current: initValue });
-  // index++;
+  let localIndex = index;
+  index++;
+
+  let target = null;
+
+  for (let i = 0; i < refArr.length; i++) {
+    if (refArr[i].current == initValue) {
+      target = i;
+      break;
+    }
+  }
+
+  // refArr.find((item: any, index: number) => {
+  //   if (item.current == initValue) {
+  //     target = index;
+  //     return true;
+  //   }
+  // });
+
+  if (target !== null) {
+    localIndex = target;
+    index = localIndex + 1;
+  } else {
+    refArr.push({ current: initValue });
+  }
+
   return refArr[localIndex];
 }
-
-// export function useMyRef<T>(initValue: T | null) {
-//   return { current: initValue };
-// }
