ec6baed4cdb2c75ea85feaac944b3a671d5efaa8
rowanna <rowanna@rowannaui-MacBookAir.local>
2024-06-18 23:08:05 +0900

    feat: 1주차 기본과제 제출

--- packages/assignment-1/src/basic/PureComponentTest.tsx
+++ packages/assignment-1/src/basic/PureComponentTest.tsx
@@ -1,7 +1,21 @@
-import { useState } from "react";
-import { Cat, Dog } from "./PureComponentTest.components.tsx";
+import { useState, memo } from "react";
+import { repeatBarked, repeatMeow } from "./UseMemoTest.utils.ts";
+// import { Cat, Dog } from "./PureComponentTest.components.tsx";
 
+type TCryingProps = {
+  crying: number;
+}
+const Dog = memo(function Dog({ crying }: TCryingProps) {
+  return (
+    <p data-testid="dog">강아지 "{repeatBarked(crying)}"</p>
+  );
+})
 
+const Cat = memo(function Cat({ crying }: TCryingProps) {
+  return (
+    <p data-testid="cat">고양이 "{repeatMeow(crying)}"</p>
+  );
+})
 // NOTE: 다른 파일은 수정하지 않고, 현재 파일만 수정하여 문제를 해결해주세요.
 export default function PureComponentTest() {
   const [meowCount, setMeowCount] = useState(1);
