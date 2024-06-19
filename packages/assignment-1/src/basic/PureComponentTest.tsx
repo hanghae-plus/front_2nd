d8a6f38f6668cdd9655efdd2a1d2d6ee93fb92d0
zhwltlr <vyoungv7340@naver.com>
2024-06-19 14:18:53 +0900

    solve basic assignment

--- packages/assignment-1/src/basic/PureComponentTest.tsx
+++ packages/assignment-1/src/basic/PureComponentTest.tsx
@@ -1,6 +1,21 @@
-import { useState } from "react";
+import { memo, useState } from "react";
 import { Cat, Dog } from "./PureComponentTest.components.tsx";
 
+type CatProps = {
+  crying: number;
+};
+
+type DogProps = {
+  crying: number;
+};
+
+const PureCatComponent = memo(({ crying }: CatProps) => {
+  return <Cat crying={crying} />;
+});
+
+const PureDogComponent = memo(({ crying }: DogProps) => {
+  return <Dog crying={crying} />;
+});
 
 // NOTE: 다른 파일은 수정하지 않고, 현재 파일만 수정하여 문제를 해결해주세요.
 export default function PureComponentTest() {
@@ -9,10 +24,14 @@ export default function PureComponentTest() {
 
   return (
     <div>
-      <Cat crying={meowCount}/>
-      <Dog crying={barkedCount}/>
-      <button data-testid="meow" onClick={() => setMeowCount(n => n + 1)}>야옹</button>
-      <button data-testid="bark" onClick={() => setBarkedCount(n => n + 1)}>멍멍</button>
+      <PureCatComponent crying={meowCount} />
+      <PureDogComponent crying={barkedCount} />
+      <button data-testid="meow" onClick={() => setMeowCount((n) => n + 1)}>
+        야옹
+      </button>
+      <button data-testid="bark" onClick={() => setBarkedCount((n) => n + 1)}>
+        멍멍
+      </button>
     </div>
   );
 }
