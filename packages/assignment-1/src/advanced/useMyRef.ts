06339f3298196d402130855f4c3ab76b9eafbebe
Rachel <borapark13@gmail.com>
2024-06-19 21:37:32 +0900

    comment: 불필요 주석 삭제

--- packages/assignment-1/src/advanced/useMyRef.ts
+++ packages/assignment-1/src/advanced/useMyRef.ts
@@ -3,6 +3,3 @@ import { useMemo } from "react";
 export function useMyRef<T>(initValue: T | null) {
   return useMemo(() => ({ current: initValue }), []);
 }
-
-//useMemo는 이전값이랑 비교를 해서 이전값이랑 같으면 렌더링을 안해
-// 이전값이랑 다르면 렌더링을 해
