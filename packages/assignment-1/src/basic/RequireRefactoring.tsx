6376dd9363f3c162e3bedcb5adcb0554ebe338be
YeeCh <livelch012@gmail.com>
2024-06-19 09:41:52 +0000

    Update: 1차과제

--- packages/assignment-1/src/basic/RequireRefactoring.tsx
+++ packages/assignment-1/src/basic/RequireRefactoring.tsx
@@ -1,13 +1,30 @@
-import { ComponentProps, memo, PropsWithChildren } from "react";
-
+import { ComponentProps, memo, PropsWithChildren } from "react"; 
+ 
 type Props = {
   countRendering?: () => void;
 }
 
+//React.memo(Component, [areEqual(prevProps, nextProps)]);
+/**
+ function moviePropsAreEqual(prevMovie, nextMovie) {
+  return (
+    prevMovie.title === nextMovie.title &&
+    prevMovie.releaseDate === nextMovie.releaseDate
+  );
+}
+ */
+const propsAreEqual = (prevProps, nextProps) => {
+  return prevProps.children === nextProps.children 
+      && prevProps.countRendering === nextProps.countRendering
+      && prevProps.style?.width === nextProps.style?.width
+      && prevProps.style?.height === nextProps.style?.height;
+}
+
+
 const PureComponent = memo(({ children, countRendering, ...props }: PropsWithChildren<ComponentProps<'div'> & Props>) => {
-  countRendering?.();
+  countRendering?.(); 
   return <div {...props}>{children}</div>
-})
+}, propsAreEqual)
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 let outerCount = 1;
@@ -19,6 +36,7 @@ const handleClick = () => {
 };
 
 export default function RequireRefactoring({ countRendering }: Props) {
+ 
   return (
     <PureComponent
       style={style}
