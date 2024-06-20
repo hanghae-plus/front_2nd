0b414f2ee3ebb1225ca22794d9dccf1c933ea216
Raihyul <tlompy1104@gmail.com>
2024-06-20 22:15:01 +0900

    [basic] useCallback
    feat: useCallback 적용하기
    - useCallback를 적용하여 불필요한 렌더링 방지하기
    - 참고: (컴포넌트의 리렌더링 건너뛰기)[https://ko.react.dev/reference/react/useCallback#skipping-re-rendering-of-components]

--- packages/assignment-1/src/basic/UseCallbackTest.tsx
+++ packages/assignment-1/src/basic/UseCallbackTest.tsx
@@ -1,16 +1,23 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { BarkButton, MeowButton } from "./UseCallbackTest.components.tsx";
 
 export default function UseCallbackTest() {
   const [meowCount, setMeowCount] = useState(0);
   const [barkedCount, setBarkedCount] = useState(0);
 
+  const changeMeowCount = useCallback(() => {
+    setMeowCount((n) => n + 1);
+  }, []);
+  const changeBarkedCount = useCallback(() => {
+    setBarkedCount((n) => n + 1);
+  }, []);
+
   return (
     <div>
       <p data-testid="cat">meowCount {meowCount}</p>
       <p data-testid="dog">barkedCount {barkedCount}</p>
-      <MeowButton onClick={() => setMeowCount(n => n + 1)}/>
-      <BarkButton onClick={() => setBarkedCount(n => n + 1)}/>
+      <MeowButton onClick={changeMeowCount} />
+      <BarkButton onClick={changeBarkedCount} />
     </div>
   );
 }
