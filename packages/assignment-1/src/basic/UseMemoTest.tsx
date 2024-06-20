2f75501d779bd06988bebfb962f6dab5441268d1
BUMSEOK SEO <sbseok12@gmail.com>
2024-06-20 22:35:13 +0900

    fix : useMemo - useMemo를 사용하여 지정한 상태값이 변경될 때만 함수를 호출

--- packages/assignment-1/src/basic/UseMemoTest.tsx
+++ packages/assignment-1/src/basic/UseMemoTest.tsx
@@ -1,12 +1,19 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { repeatBarked, repeatMeow } from "./UseMemoTest.utils.ts";
 
 export default function UseMemoTest() {
   const [meowCount, setMeowCount] = useState(1);
   const [barkedCount, setBarkedCount] = useState(1);
 
-  const meow = repeatMeow(meowCount);
-  const bark = repeatBarked(barkedCount);
+  // useMemo를 사용하여 컴포넌트가 리렌더링 되더라도,
+  // 상태값(meowCount, barkedCount)이 변경되지 않으면 함수를 다시 호출하지 않고 이전에 생성된 문자열을 사용
+
+  // meowCount, barkedCount가 변경될 때만 함수가 호출
+  // 변경 X => 이전에 생성된 문자열을 사용
+  // 변경 O => 변경되면 새로운 문자열을 생성
+  
+  const meow = useMemo(() => repeatMeow(meowCount), [meowCount]); 
+  const bark = useMemo(() => repeatBarked(barkedCount), [barkedCount]);
 
   return (
     <div>
