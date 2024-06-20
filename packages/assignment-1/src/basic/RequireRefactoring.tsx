d593d10880efef57cc5fc67ea9f6967c578c92bf
eumcloud <81782468+eumcloud@users.noreply.github.com>
2024-06-20 23:45:20 +0900

    Update RequireRefactoring.tsx
    basck-5

--- packages/assignment-1/src/basic/RequireRefactoring.tsx
+++ packages/assignment-1/src/basic/RequireRefactoring.tsx
@@ -1,41 +1,28 @@
-import { ComponentProps, memo, PropsWithChildren } from "react"; 
- 
+import { ComponentProps, memo, PropsWithChildren } from "react";
+
 type Props = {
   countRendering?: () => void;
 }
 
-//React.memo(Component, [areEqual(prevProps, nextProps)]);
-/**
- function moviePropsAreEqual(prevMovie, nextMovie) {
-  return (
-    prevMovie.title === nextMovie.title &&
-    prevMovie.releaseDate === nextMovie.releaseDate
-  );
-}
- */
-const propsAreEqual = (prevProps, nextProps) => {
-  return prevProps.children === nextProps.children 
-      && prevProps.countRendering === nextProps.countRendering
-      && prevProps.style?.width === nextProps.style?.width
-      && prevProps.style?.height === nextProps.style?.height;
-}
-
-
 const PureComponent = memo(({ children, countRendering, ...props }: PropsWithChildren<ComponentProps<'div'> & Props>) => {
-  countRendering?.(); 
+  countRendering?.();
   return <div {...props}>{children}</div>
-}, propsAreEqual)
+})
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-let outerCount = 1
-
-// useMemo, useCallback 등을 사용하지 않고 이 컴포넌트를 개선해보세요.
-export default function RequireRefactoring({ countRendering }: Props) {
+let outerCount = 1;
  
+/** */
+const style = { width: '100px', height: '100px' };
+const handleClick = () => {
+  outerCount += 1;
+};
+
+export default function RequireRefactoring({ countRendering }: Props) {
   return (
     <PureComponent
-      style={{ width: '100px', height: '100px' }}
-      onClick={() => {outerCount += 1;}}
+      style={style}
+      onClick={handleClick}
       countRendering={countRendering}
     >
       test component
