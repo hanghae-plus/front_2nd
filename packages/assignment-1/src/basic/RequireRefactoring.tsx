83d95994df3db2b3bdddb13e0a0ea4464f8cffad
jiyeon87 <jenny87879@gmail.com>
2024-06-20 19:26:38 +0900

    Week1 - Basic 과제 해결

--- packages/assignment-1/src/basic/RequireRefactoring.tsx
+++ packages/assignment-1/src/basic/RequireRefactoring.tsx
@@ -2,22 +2,34 @@ import { ComponentProps, memo, PropsWithChildren } from "react";
 
 type Props = {
   countRendering?: () => void;
-}
+};
 
-const PureComponent = memo(({ children, countRendering, ...props }: PropsWithChildren<ComponentProps<'div'> & Props>) => {
-  countRendering?.();
-  return <div {...props}>{children}</div>
-})
+const PureComponent = memo(
+  ({
+    children,
+    countRendering,
+    ...props
+  }: PropsWithChildren<ComponentProps<"div"> & Props>) => {
+    countRendering?.();
+    return <div {...props}>{children}</div>;
+  }
+);
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-let outerCount = 1
+let outerCount = 1;
+
+// 스타일과 핸들러를 컴포넌트 외부에서 정의
+const fixedStyle = { width: "100px", height: "100px" };
+const handleClick = () => {
+  outerCount += 1;
+};
 
 // useMemo, useCallback 등을 사용하지 않고 이 컴포넌트를 개선해보세요.
 export default function RequireRefactoring({ countRendering }: Props) {
   return (
     <PureComponent
-      style={{ width: '100px', height: '100px' }}
-      onClick={() => {outerCount += 1;}}
+      style={fixedStyle}
+      onClick={handleClick}
       countRendering={countRendering}
     >
       test component
