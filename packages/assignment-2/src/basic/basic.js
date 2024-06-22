c8fa974a761ceaec3f11a5be41cc5ac235114338
seojunhwan <seojunhwan@kakao.com>
2024-06-23 01:26:10 +0900

    feat: deep equal

--- packages/assignment-2/src/basic/basic.js
+++ packages/assignment-2/src/basic/basic.js
@@ -32,7 +32,36 @@ export function shallowEquals(target1, target2) {
 }
 
 export function deepEquals(target1, target2) {
-  return target1 === target2;
+  if (target1 === target2) {
+    return true;
+  }
+
+  if (typeof target1 !== 'object' || typeof target2 !== 'object' || 
+      target1 === null || target2 === null) {
+    return false;
+  }
+
+  if ((target1 instanceof Number && target2 instanceof Number) ||
+      (target1 instanceof String && target2 instanceof String)) {
+    return false;
+  }
+
+  if (Array.isArray(target1) && Array.isArray(target2)) {
+    return target1.length === target2.length && target1.every((value, index) => deepEquals(value, target2[index]));
+  }
+
+  if (target1.constructor !== Object || target2.constructor !== Object) {
+    return target1.constructor === target2.constructor;
+  }
+
+  const target1Keys = Object.keys(target1);
+  const target2Keys = Object.keys(target2);
+
+  if (target1Keys.length !== target2Keys.length) {
+    return false;
+  }
+
+  return target1Keys.every(key => deepEquals(target1[key], target2[key]));
 }
 
 
