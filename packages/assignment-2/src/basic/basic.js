6cf4080fcfe9ce40f1da947f610e5b5fb6cf5f12
seojunhwan <seojunhwan@kakao.com>
2024-06-23 01:20:28 +0900

    feat: shallow equal 구현

--- packages/assignment-2/src/basic/basic.js
+++ packages/assignment-2/src/basic/basic.js
@@ -1,5 +1,39 @@
 export function shallowEquals(target1, target2) {
-  return target1 === target2;
+  if (target1 === target2) {
+    return true;
+  }
+
+  if (typeof target1 !== 'object' || typeof target2 !== 'object') {
+    return false;
+  }
+
+  if (target1 instanceof Number && target2 instanceof Number ||
+     target1 instanceof String && target2 instanceof String) {
+    return false;
+  }
+
+  if (Array.isArray(target1) && Array.isArray(target2)) {
+    return target1.length === target2.length && target1.every((value, index) => value === target2[index]);
+  }
+
+  if (target1.constructor !== Object && target2.constructor !== Object) {
+    return target1.constructor === target2.constructor;
+  }
+
+  const target1Keys = Object.keys(target1);
+  const target2Keys = Object.keys(target2);
+
+  if (target1Keys.length !== target2Keys.length) {
+    return false;
+  }
+
+  for (let i = 0; i < target1Keys.length; i++) {
+    if (target1[target1Keys[i]] !== target2[target2Keys[i]]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 export function deepEquals(target1, target2) {
