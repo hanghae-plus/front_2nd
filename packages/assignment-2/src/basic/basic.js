8afad5432f48858ed5a4ee7f2eda7daebdff94ba
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2024-06-22 21:13:40 +0900

    refactor : shallowEquals

--- packages/assignment-2/src/basic/basic.js
+++ packages/assignment-2/src/basic/basic.js
@@ -1,52 +1,39 @@
 export function shallowEquals(target1, target2) {
-  return target1 === target2;
+  // console.log(target1, target2);
+  // console.log(Object.is(target1, target2));
+  return Object.is(target1, target2);
+  // return target1 === target2;
 }
 
 export function deepEquals(target1, target2) {
   return target1 === target2;
 }
 
-
 export function createNumber1(n) {
-  return n;
+  return Number(n);
 }
 
 export function createNumber2(n) {
-  return n;
+  return Number(n);
+  // return n;
 }
 
 export function createNumber3(n) {
   return n;
 }
 
-export class CustomNumber {
-
-}
+export class CustomNumber {}
 
 export function createUnenumerableObject(target) {
   return target;
 }
 
-export function forEach(target, callback) {
+export function forEach(target, callback) {}
 
-}
+export function map(target, callback) {}
 
-export function map(target, callback) {
+export function filter(target, callback) {}
 
-}
+export function every(target, callback) {}
 
-export function filter(target, callback) {
-
-}
-
-
-export function every(target, callback) {
-
-}
-
-export function some(target, callback) {
-
-}
-
-
-
+export function some(target, callback) {}
