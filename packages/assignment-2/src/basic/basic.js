ba65ab364d4e53007d42d04fb7228c9e82b40dde
LucetTin5 <lucettin5@gmail.com>
2024-06-24 14:48:32 +0900

    basic-2: deepEquals

--- packages/assignment-2/src/basic/basic.js
+++ packages/assignment-2/src/basic/basic.js
@@ -51,9 +51,32 @@ export function shallowEquals(target1, target2) {
 }
 
 export function deepEquals(target1, target2) {
+  if (isClassInstance(target1) || isClassInstance(target2)) {
+    return target1 === target2;
+  }
+  if (
+    Object.prototype.toString.call(target1).split(" ")[1].includes("Object") ||
+    Object.prototype.toString.call(target1).split(" ")[1].includes("Array")
+  ) {
+    const keys1 = Object.keys(target1);
+    const keys2 = Object.keys(target2);
+
+    if (keys1.length !== keys2.length) {
+      return false;
+    }
+
+    for (let i = 0; i < keys1.length; i++) {
+      const currentKey = keys1[i];
+      // object / array 내에 다시 object, array가 존재하는 경우 해당 object, array를 다시 비교하기 위해 재귀 호출
+      if (!deepEquals(target1[currentKey], target2[currentKey])) {
+        return false;
+      }
+    }
+
+    return true;
+  }
   return target1 === target2;
 }
-
 
 export function createNumber1(n) {
   return n;
