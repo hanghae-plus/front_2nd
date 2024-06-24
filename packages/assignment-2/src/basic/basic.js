684d5196afc816fd6ccf91c710c2374d08338897
HYEWON <hiimhyewon@naver.com>
2024-06-24 20:25:38 +0900

    [추가] shallowEquals, deepEquals 완료

--- packages/assignment-2/src/basic/basic.js
+++ packages/assignment-2/src/basic/basic.js
@@ -1,11 +1,54 @@
 export function shallowEquals(target1, target2) {
+  const typeOf = (target) => {
+    const targetType =
+      target?.constructor.name ?? Object.prototype.toString.call(target);
+    return targetType;
+  };
+
+  if (typeOf(target1) !== typeOf(target2)) {
+    return false;
+  }
+
+  if (typeOf(target1) === "Array") {
+    if (target1.length !== target2.length) {
+      return false;
+    }
+    return target1.every((el, index) => el === target2[index]);
+  } else if (typeOf(target1) === "Object") {
+    return Object.keys(target1).every((key) => target1[key] === target2[key]);
+  }
+
   return target1 === target2;
 }
 
 export function deepEquals(target1, target2) {
+  const typeOf = (target) => {
+    const targetType =
+      target?.constructor.name ?? Object.prototype.toString.call(target);
+    return targetType;
+  };
+
+  if (typeOf(target1) !== typeOf(target2)) {
+    return false;
+  }
+
+  if (typeOf(target1) === "Array") {
+    if (target1.length !== target2.length) {
+      return false;
+    }
+    return target1.every((el, index) => deepEquals(el, target2[index]));
+  } else if (typeOf(target1) === "Object") {
+    if (Object.keys(target1).length !== Object.keys(target2).length) {
+      return false;
+    }
+
+    return Object.keys(target1).every((key) =>
+      deepEquals(target1[key], target2[key]),
+    );
+  }
+
   return target1 === target2;
 }
-
 
 export function createNumber1(n) {
   return n;
@@ -19,34 +62,18 @@ export function createNumber3(n) {
   return n;
 }
 
-export class CustomNumber {
-
-}
+export class CustomNumber {}
 
 export function createUnenumerableObject(target) {
   return target;
 }
 
-export function forEach(target, callback) {
+export function forEach(target, callback) {}
 
-}
+export function map(target, callback) {}
 
-export function map(target, callback) {
+export function filter(target, callback) {}
 
-}
+export function every(target, callback) {}
 
-export function filter(target, callback) {
-
-}
-
-
-export function every(target, callback) {
-
-}
-
-export function some(target, callback) {
-
-}
-
-
-
+export function some(target, callback) {}
