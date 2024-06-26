cab32215948ae8c6bb35650f58aaad0101bed759
chhw130 <527coco@naver.com>
2024-06-26 20:03:47 +0900

    feat : memo함수

--- packages/assignment-2/src/advanced/advanced.jsx
+++ packages/assignment-2/src/advanced/advanced.jsx
@@ -1,13 +1,50 @@
 import { createContext, useContext, useState } from "react";
 
-export const memo1 = (fn) => fn();
+const map = new Map();
+const dependencyMap = new Map();
 
-export const memo2 = (fn) => fn();
+/**map에 함수 명 Mapping하여 있는 값이면 기존 값 반환하는 함수 */
+export const memo1 = (fn) => {
+  const key = fn.name;
 
+  if (map.get(key)) {
+    return map.get(key);
+  }
+
+  map.set(key, fn());
+
+  return map.get(key);
+};
+
+/**의존성 배열?에 따라 값 반환하는 함수 */
+export const memo2 = (fn, dependencyArr) => {
+  const mapKey = fn.name;
+
+  const currentDependency = dependencyMap.get(mapKey);
+
+  /**
+   * 기존 map의 의존성 배열 요소들과 현재 들어온 의존성 배열 요소가
+   * 하나라도 바뀌었는지 체크
+   */
+  const isNotChange = currentDependency?.every(
+    (ele, idx) => ele === dependencyArr[idx]
+  );
+
+  //바뀌지 않고 값이 이미 있다면 기존 값 리턴
+  if (map.get(mapKey) && isNotChange) {
+    return map.get(mapKey);
+  }
+
+  //아니라면 새로 값을 set
+  map.set(mapKey, fn());
+  dependencyMap.set(mapKey, dependencyArr);
+
+  return map.get(mapKey);
+};
 
 export const useCustomState = (initValue) => {
   return useState(initValue);
-}
+};
 
 const textContextDefaultValue = {
   user: null,
@@ -27,36 +64,27 @@ export const TestContextProvider = ({ children }) => {
     <TestContext.Provider value={{ value, setValue }}>
       {children}
     </TestContext.Provider>
-  )
-}
+  );
+};
 
 const useTestContext = () => {
   return useContext(TestContext);
-}
+};
 
 export const useUser = () => {
   const { value, setValue } = useTestContext();
 
-  return [
-    value.user,
-    (user) => setValue({ ...value, user })
-  ];
-}
+  return [value.user, (user) => setValue({ ...value, user })];
+};
 
 export const useCounter = () => {
   const { value, setValue } = useTestContext();
 
-  return [
-    value.count,
-    (count) => setValue({ ...value, count })
-  ];
-}
+  return [value.count, (count) => setValue({ ...value, count })];
+};
 
 export const useTodoItems = () => {
   const { value, setValue } = useTestContext();
 
-  return [
-    value.todoItems,
-    (todoItems) => setValue({ ...value, todoItems })
-  ];
-}
+  return [value.todoItems, (todoItems) => setValue({ ...value, todoItems })];
+};
