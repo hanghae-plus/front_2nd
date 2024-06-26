61bcf43d9af0599278765d05068ee5746c2eedf1
2pandi <coder.2pandi@gmail.com>
2024-06-26 13:00:21 +0900

    basic 풀이

--- packages/assignment-2/src/basic/basic.js
+++ packages/assignment-2/src/basic/basic.js
@@ -1,52 +1,225 @@
 export function shallowEquals(target1, target2) {
-  return target1 === target2;
+  if (target1 === target2) return true;
+  if (typeof target1 !== typeof target2) return false;
+  if (target1 == null || target2 == null) {
+    return false;
+  }
+  if (Array.isArray(target1) && Array.isArray(target2)) {
+    if (target1.length !== target2.length) return false;
+    for (let i = 0; i < target1.length; i++) {
+      if (target1[i] !== target2[i]) return false;
+    }
+    return true;
+  }
+  if (
+    target1.__proto__.constructor === Object &&
+    target2.__proto__.constructor === Object
+  ) {
+    const isKeyEqual = shallowEquals(
+      Object.keys(target1),
+      Object.keys(target2)
+    );
+    const isValueEqual = shallowEquals(
+      Object.values(target1),
+      Object.values(target2)
+    );
+
+    return isKeyEqual && isValueEqual;
+  }
+  return false;
 }
 
 export function deepEquals(target1, target2) {
-  return target1 === target2;
+  if (target1 === target2) return true;
+  if (typeof target1 !== typeof target2) return false;
+  if (target1 == null || target2 == null) {
+    return false;
+  }
+  if (Array.isArray(target1) && Array.isArray(target2)) {
+    if (target1.length !== target2.length) return false;
+    const trueSet = new Set();
+    for (let i = 0; i < target1.length; i++) {
+      if (Array.isArray(target1[i]) && Array.isArray(target2[i])) {
+        trueSet.add(deepEquals(target1[i], target2[i]));
+        continue;
+      }
+      if (
+        target1[i].__proto__.constructor === Object &&
+        target2[i].__proto__.constructor === Object
+      ) {
+        const isKeyEqual = deepEquals(
+          Object.keys(target1[i]),
+          Object.keys(target2[i])
+        );
+        const isValueEqual = deepEquals(
+          Object.values(target1[i]),
+          Object.values(target2[i])
+        );
+
+        trueSet.add(isKeyEqual && isValueEqual);
+        continue;
+      }
+      if (target1[i] !== target2[i]) {
+        trueSet.add(false);
+        return false;
+      }
+    }
+    return !trueSet.has(false);
+  }
+  if (
+    target1.__proto__.constructor === Object &&
+    target2.__proto__.constructor === Object
+  ) {
+    const isKeyEqual = deepEquals(Object.keys(target1), Object.keys(target2));
+    const isValueEqual = deepEquals(
+      Object.values(target1),
+      Object.values(target2)
+    );
+
+    return isKeyEqual && isValueEqual;
+  }
+  return false;
 }
 
-
 export function createNumber1(n) {
-  return n;
+  return new Number(n);
 }
 
 export function createNumber2(n) {
-  return n;
+  return new String(n);
 }
 
 export function createNumber3(n) {
-  return n;
+  return {
+    valueOf() {
+      return n;
+    },
+    toString() {
+      return n;
+    },
+    toJSON() {
+      return `this is createNumber3 => ${n}`;
+    },
+  };
 }
 
-export class CustomNumber {
+const customNumberCache = new Map();
 
+export class CustomNumber {
+  constructor(n) {
+    if (customNumberCache.has(n)) return customNumberCache.get(n);
+    this.n = n;
+    customNumberCache.set(n, this);
+  }
+
+  valueOf() {
+    return this.n;
+  }
+
+  toString() {
+    return this.n;
+  }
+
+  toJSON() {
+    return `${this.n}`;
+  }
 }
 
 export function createUnenumerableObject(target) {
-  return target;
+  const newObj = {};
+
+  for (const key in target) {
+    if (target.hasOwnProperty(key)) {
+      Object.defineProperty(newObj, key, {
+        value: target[key],
+        enumerable: false,
+        writable: true,
+        configurable: true,
+      });
+    }
+  }
+
+  return newObj;
 }
 
 export function forEach(target, callback) {
+  if (target.__proto__.constructor === Object) {
+    const keys = Object.getOwnPropertyNames(target);
+    for (let i = 0; i < keys.length; i++) {
+      callback(target[keys[i]], keys[i]);
+    }
+    return;
+  }
 
+  for (let i = 0; i < target.length; i++) {
+    callback(target[i], i);
+  }
 }
 
 export function map(target, callback) {
+  if (target.__proto__.constructor === Object) {
+    const newTarget = {};
+    const keys = Object.getOwnPropertyNames(target);
+    for (let i = 0; i < keys.length; i++) {
+      newTarget[keys[i]] = callback(target[keys[i]]);
+    }
+    return newTarget;
+  }
+  const newTarget = [];
+  for (let i = 0; i < target.length; i++) {
+    newTarget.push(callback(target[i]));
+  }
 
+  return newTarget;
 }
 
 export function filter(target, callback) {
-
+  if (target.__proto__.constructor === Object) {
+    const newTarget = {};
+    const keys = Object.getOwnPropertyNames(target);
+    for (let i = 0; i < keys.length; i++) {
+      if (callback(target[keys[i]])) {
+        newTarget[keys[i]] = target[keys[i]];
+      }
+    }
+    return newTarget;
+  }
+  const newTarget = [];
+  for (let i = 0; i < target.length; i++) {
+    if (callback(target[i])) {
+      newTarget.push(target[i]);
+    }
+  }
+  return newTarget;
 }
 
-
 export function every(target, callback) {
+  if (target.__proto__.constructor === Object) {
+    const keys = Object.getOwnPropertyNames(target);
+    for (let i = 0; i < keys.length; i++) {
+      if (!callback(target[keys[i]])) return false;
+    }
+    return true;
+  }
 
+  for (let i = 0; i < target.length; i++) {
+    if (!callback(target[i])) return false;
+  }
+
+  return true;
 }
 
 export function some(target, callback) {
+  if (target.__proto__.constructor === Object) {
+    const keys = Object.getOwnPropertyNames(target);
+    for (let i = 0; i < keys.length; i++) {
+      if (callback(target[keys[i]])) return true;
+    }
+    return false;
+  }
 
+  for (let i = 0; i < target.length; i++) {
+    if (callback(target[i])) return true;
+  }
+  return false;
 }
-
-
-
