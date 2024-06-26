7533cfdc6426127174b7e310d17ea57244f3e8e1
Joie-Kim <joie.huiju@gmail.com>
2024-06-26 22:20:53 +0900

    [2주차] 심화과제 - 값을 캐시하기

--- packages/assignment-2/src/advanced/advanced.jsx
+++ packages/assignment-2/src/advanced/advanced.jsx
@@ -1,13 +1,51 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState } from 'react';
 
-export const memo1 = (fn) => fn();
+/** 함수 객체를 키로 하여 함수 실행 결과를 저장하는 맵
+ *
+ * @link https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/WeakMap
+ */
+const cache = new WeakMap();
 
-export const memo2 = (fn) => fn();
+/**
+ * memo1
+ *
+ * 함수가 달라지지 않으면 새로 생성하지 않음
+ */
+export const memo1 = (fn) => {
+  if (!cache.has(fn)) {
+    cache.set(fn, fn());
+  }
 
+  return cache.get(fn);
+};
+
+/**
+ * memo2
+ *
+ * 함수 또는 의존성 배열이 달라지지 않으면 새로 생성하지 않음
+ */
+export const memo2 = (fn, dependencies) => {
+  if (!cache.has(fn)) {
+    cache.set(fn, { result: fn(), dependencies });
+  }
+
+  const cached = cache.get(fn);
+  const isSame =
+    dependencies.length === cached.dependencies.length &&
+    dependencies.every((newDep, i) => newDep === cached.dependencies[i]);
+
+  if (!isSame) {
+    const result = fn();
+    cache.set(fn, { result, dependencies });
+    return result;
+  }
+
+  return cached.result;
+};
 
 export const useCustomState = (initValue) => {
   return useState(initValue);
-}
+};
 
 const textContextDefaultValue = {
   user: null,
@@ -27,36 +65,27 @@ export const TestContextProvider = ({ children }) => {
     <TestContext.Provider value={{ value, setValue }}>
       {children}
     </TestContext.Provider>
-  )
-}
+  );
+};
 
 const useTestContext = () => {
   return useContext(TestContext);
-}
+};
 
 export const useUser = () => {
   const { value, setValue } = useTestContext();
 
-  return [
-    value.user,
-    (user) => setValue({ ...value, user })
-  ];
-}
+  return [value.user, (user) => setValue({ ...value, user })];
+};
 
 export const useCounter = () => {
   const { value, setValue } = useTestContext();
 
-  return [
-    value.count,
-    (count) => setValue({ ...value, count })
-  ];
-}
+  return [value.count, (count) => setValue({ ...value, count })];
+};
 
 export const useTodoItems = () => {
   const { value, setValue } = useTestContext();
 
-  return [
-    value.todoItems,
-    (todoItems) => setValue({ ...value, todoItems })
-  ];
-}
+  return [value.todoItems, (todoItems) => setValue({ ...value, todoItems })];
+};
