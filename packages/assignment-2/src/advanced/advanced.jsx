527218ae37bcede8be8b52316f3b307306a5a67b
Huiju-Kim <huijoo5575@golfzon.com>
2024-06-27 16:22:24 +0900

    [2주차] 심화과제 - 전역 상태 참조(1)
    reduce를 사용해 컨텍스트를 분리했으나, 렌더링이 같이 되는 이슈가 아직 있음..

--- packages/assignment-2/src/advanced/advanced.jsx
+++ packages/assignment-2/src/advanced/advanced.jsx
@@ -1,10 +1,16 @@
-import { createContext, useContext, useRef, useState } from 'react';
+import { createContext, useContext, useReducer, useRef, useState } from "react";
 
+// #region Q1, Q2) 메모
 /** 함수 객체를 키로 하여 함수 실행 결과를 저장하는 맵
  *
  * @link https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/WeakMap
  */
 const cache = new WeakMap();
+
+// type Fn = () => unknown;
+
+// type Memo1 = (fn: Fn) => unknown;
+// type Memo2 = (fn: Fn, dependencies: unknown[]) => unknown;
 
 /**
  * memo1
@@ -42,7 +48,9 @@ export const memo2 = (fn, dependencies) => {
 
   return cached.result;
 };
+// #endregion
 
+// #region Q3) 커스텀 상태
 export const useCustomState = (initValue) => {
   const [state, setState] = useState(initValue);
   const cacheRef = useRef(JSON.stringify(initValue));
@@ -63,6 +71,36 @@ export const useCustomState = (initValue) => {
 
   return [state, setCustomState];
 };
+// #endregion
+
+// #region Q4) 전역 상태 참조
+// INFO: 컨텍스트를 분리하는 방법을 생각했으나, 컨텍스트가 더 많아질 경우를 생각해 useReducer를 사용해 해결
+
+// /** 상태 타입 */
+// interface State {
+//   user: { name: string } | null;
+//   todoItems: { id: number; content: string; completed: boolean }[];
+//   count: number;
+// }
+
+// /** 액션 타입 */
+// type Action =
+//   | { type: "SET_USER"; payload: { name: string } | null }
+//   | {
+//       type: "SET_TODO_ITEMS";
+//       payload: { id: number; content: string; completed: boolean }[];
+//     }
+//   | { type: "SET_COUNT"; payload: number };
+
+// /** 리듀서 타입 */
+// type Reducer = (state: State, action: Action) => State;
+
+// /** provider prop 타입 */
+// interface ProviderProps {
+//   children: ReactNode;
+// }
+
+//
 
 const textContextDefaultValue = {
   user: null,
@@ -70,39 +108,83 @@ const textContextDefaultValue = {
   count: 0,
 };
 
-export const TestContext = createContext({
-  value: textContextDefaultValue,
-  setValue: () => null,
-});
+// INFO: state와 dispatch 중 하나만 사용하는 컴포넌트가 불필요하게 리렌더링 되지 않도록 분리 (현재 테스트 코드엔 없지만..)
+
+/** 상태 값(state) 컨텍스트 */
+// const StateContext = createContext<State | undefined>(undefined);
+const StateContext = createContext(undefined);
+
+/** 상태 값 변경 함수(dispatch) 컨텍스트 */
+// const DispatchContext = createContext<Dispatch<Action> | undefined>(undefined);
+const DispatchContext = createContext(undefined);
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SET_USER":
+      return { ...state, user: action.payload };
+    case "SET_TODO_ITEMS":
+      return { ...state, todoItems: action.payload };
+    case "SET_COUNT":
+      return { ...state, count: action.payload };
+    default:
+      throw new Error(`Unhandled action type`);
+  }
+};
 
 export const TestContextProvider = ({ children }) => {
-  const [value, setValue] = useState(textContextDefaultValue);
+  const [state, dispatch] = useReducer(reducer, textContextDefaultValue);
 
   return (
-    <TestContext.Provider value={{ value, setValue }}>
-      {children}
-    </TestContext.Provider>
+    <StateContext.Provider value={state}>
+      <DispatchContext.Provider value={dispatch}>
+        {children}
+      </DispatchContext.Provider>
+    </StateContext.Provider>
   );
 };
 
-const useTestContext = () => {
-  return useContext(TestContext);
+export const useStateContext = () => {
+  const context = useContext(StateContext);
+  if (context === undefined) {
+    throw new Error("There is no context!");
+  }
+  return context;
+};
+
+export const useDispatchContext = () => {
+  const context = useContext(DispatchContext);
+  if (context === undefined) {
+    throw new Error("There is no context!");
+  }
+  return context;
 };
 
 export const useUser = () => {
-  const { value, setValue } = useTestContext();
-
-  return [value.user, (user) => setValue({ ...value, user })];
-};
-
-export const useCounter = () => {
-  const { value, setValue } = useTestContext();
-
-  return [value.count, (count) => setValue({ ...value, count })];
+  const state = useStateContext();
+  const dispatch = useDispatchContext();
+  const setUser = (user) => {
+    console.log("here user!");
+    return dispatch({ type: "SET_USER", payload: user });
+  };
+  return [state.user, setUser];
 };
 
 export const useTodoItems = () => {
-  const { value, setValue } = useTestContext();
-
-  return [value.todoItems, (todoItems) => setValue({ ...value, todoItems })];
+  const state = useStateContext();
+  const dispatch = useDispatchContext();
+  const setTodoItems = (items) =>
+    dispatch({ type: "SET_TODO_ITEMS", payload: items });
+  return [state.todoItems, setTodoItems];
 };
+
+export const useCounter = () => {
+  const state = useStateContext();
+  const dispatch = useDispatchContext();
+  const setCount = (count) => {
+    console.log("here count...");
+    return dispatch({ type: "SET_COUNT", payload: count });
+  };
+  return [state.count, setCount];
+};
+
+// #endregion
