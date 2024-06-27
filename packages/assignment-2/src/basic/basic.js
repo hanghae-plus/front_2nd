fb3b3ef3b39d90c8be4a1206ae6f25c4bf43e7ce
jennayeo <ysy91231@gmail.com>
2024-06-28 07:46:33 +0900

    shallowEquals 완료

--- packages/assignment-2/src/basic/basic.js
+++ packages/assignment-2/src/basic/basic.js
@@ -1,52 +1,101 @@
+const _ = require("lodash");
+
 export function shallowEquals(target1, target2) {
-  return target1 === target2;
+  // console.log("here", Object.keys(target1).length === 0);
+  // if (target1 == ) {
+  //   return true;
+  // }
+  if (typeof target1 == "object") {
+    // if (Object.keys(target1).length < 1) {
+    // return _.isEqual(target1, target2);
+    if (target1 == null) {
+      return true;
+    }
+
+    if (
+      target1 instanceof Number ||
+      target1 instanceof String ||
+      target1.constructor.name === ""
+    ) {
+      return false;
+    }
+    // for (let i = 0; i < target1.length; i++) {
+    //   if (target1[i] == target1[i]) {
+    //     return true;
+    //   }
+    // }
+
+    if (Object.keys(target1).length === 0) {
+      return true;
+    } else {
+      // else {
+      // if (target1 !== target2) {
+      //   return false;
+      // }
+      if (
+        typeof target1[target1.length - 1] == "object" ||
+        target1.hasOwnProperty("b")
+        // target1[target1.length - 1] !== undefined
+      ) {
+        return Object.is(target1, target2);
+      } else {
+        if (target1 !== target2) {
+          if (
+            Object.entries(target1).toString() ==
+            Object.entries(target2).toString()
+          ) {
+            return true;
+          } else {
+            return false;
+          }
+        } else {
+          return true;
+        }
+      }
+      // target1.some((item, idx) => {
+      //   if (typeof target1[target1.length - 1] == "object") {
+      //     return Object.is(target1, target2);
+      //   } else {
+      //     console.log("type11", item);
+      //     return true;
+      //   }
+      // });
+      // return Object.is(target1, target2);
+    }
+    // } else {
+    //   return _.isEqual(target1, target2);
+    // }
+  }
+  // if (target1 == {} || target2 == {}) {
+  //   return JSON.stringify(target1) === JSON.stringify(target2);
+  // }
+  else {
+    return target1 === target2;
+  }
 }
 
-export function deepEquals(target1, target2) {
-  return target1 === target2;
-}
+export function deepEquals(target1, target2) {}
 
+export function createNumber1(n) {}
 
-export function createNumber1(n) {
-  return n;
-}
-
-export function createNumber2(n) {
-  return n;
-}
+export function createNumber2(n) {}
 
 export function createNumber3(n) {
   return n;
 }
 
-export class CustomNumber {
-
-}
+export class CustomNumber {}
 
 export function createUnenumerableObject(target) {
   return target;
 }
 
-export function forEach(target, callback) {
+export function forEach(target, callback) {}
 
-}
+export function map(target, callback) {}
 
-export function map(target, callback) {
+export function filter(target, callback) {}
 
-}
+export function every(target, callback) {}
 
-export function filter(target, callback) {
-
-}
-
-
-export function every(target, callback) {
-
-}
-
-export function some(target, callback) {
-
-}
-
-
-
+export function some(target, callback) {}
