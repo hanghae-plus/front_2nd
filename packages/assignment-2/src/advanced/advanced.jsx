7e9cae6292b091d28496a4a23fc1de674973ac13
rowanna <rowanna@192.168.nate.com>
2024-06-28 06:16:21 +0900

    feat: step4 과제제출

--- packages/assignment-2/src/advanced/advanced.jsx
+++ packages/assignment-2/src/advanced/advanced.jsx
@@ -1,13 +1,70 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useCallback } from "react";
 
-export const memo1 = (fn) => fn();
+export const memo1 = (fn) => {
+  const cache = Object.getPrototypeOf(a)
+  return function(arg) {
+    if (cache.has(arg)) {
+      return cache.get(arg);
+    } else {
+      const result = fn(arg);
+      cache.set(arg, result);
+      return result;
+    }
+  };
+};
 
-export const memo2 = (fn) => fn();
+export const memo2 = (fn) => {
+  const cache = new Map();
+  return function(...args) {
+    const key = JSON.stringify(args);
+    if (cache.has(key)) {
+      return cache.get(key);
+    } else {
+      const result = fn(...args);
+      cache.set(key, result);
+      return result;
+    }
+  };
+};
 
 
-export const useCustomState = (initValue) => {
-  return useState(initValue);
-}
+// 얕은 비교를 위한 함수
+const shallowEqual = (objA, objB) => {
+  if (Object.is(objA, objB)) return true;
+
+  if (typeof objA !== 'object' || objA === null || typeof objB !== 'object' || objB === null) {
+    return false;
+  }
+
+  const keysA = Object.keys(objA);
+  const keysB = Object.keys(objB);
+
+  if (keysA.length !== keysB.length) return false;
+
+  for (let i = 0; i < keysA.length; i++) {
+    if (!Object.prototype.hasOwnProperty.call(objB, keysA[i]) || !Object.is(objA[keysA[i]], objB[keysA[i]])) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+// useCustomState 훅
+export const useCustomState = (initialState) => {
+  const [state, setState] = useState(initialState);
+
+  const setCustomState = useCallback((newState) => {
+    setState(prevState => {
+      if (shallowEqual(prevState, newState)) {
+        return prevState;
+      }
+      return newState;
+    });
+  }, []);
+
+  return [state, setCustomState];
+};
 
 const textContextDefaultValue = {
   user: null,
