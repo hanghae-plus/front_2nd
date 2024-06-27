aa29af14b963b3e1995d28125b70836d6da6d65e
hiimhyewon <HWUHLyVper5mDbGxBqXPyM3omwE1DOT+Z/UNy8izQ70=>
2024-06-27 18:03:44 +0900

    [추가] advanced - memo1, memo2 완료

--- packages/assignment-2/src/advanced/advanced.jsx
+++ packages/assignment-2/src/advanced/advanced.jsx
@@ -1,13 +1,34 @@
 import { createContext, useContext, useState } from "react";
 
-export const memo1 = (fn) => fn();
+// 캐싱 기능 검증 (클로저 활용)
+export const memo1 = (() => {
+  const cache = new Map(); // 자유 변수
 
-export const memo2 = (fn) => fn();
+  // 즉시실행함수가 반환한 함수는 memo1에 할당
+  return (fn) => {
+    if (!cache.has(fn)) {
+      cache.set(fn, fn());
+    }
+    return cache.get(fn);
+  };
+})();
 
+export const memo2 = (() => {
+  const cache = new Map();
+
+  return (fn, ...args) => {
+    const key = `${fn}${args}`;
+
+    if (!cache.has(key)) {
+      cache.set(key, fn());
+    }
+    return cache.get(key);
+  };
+})();
 
 export const useCustomState = (initValue) => {
   return useState(initValue);
-}
+};
 
 const textContextDefaultValue = {
   user: null,
@@ -27,36 +48,27 @@ export const TestContextProvider = ({ children }) => {
     <TestContext.Provider value={{ value, setValue }}>
       {children}
     </TestContext.Provider>
-  )
-}
+  );
+};
 
 const useTestContext = () => {
   return useContext(TestContext);
-}
+};
 
 export const useUser = () => {
   const { value, setValue } = useTestContext();
 
-  return [
-    value.user,
-    (user) => setValue({ ...value, user })
-  ];
-}
+  return [value.user, (user) => setValue({ ...value, user })];
+};
 
 export const useCounter = () => {
   const { value, setValue } = useTestContext();
 
-  return [
-    value.count,
-    (count) => setValue({ ...value, count })
-  ];
-}
+  return [value.count, (count) => setValue({ ...value, count })];
+};
 
 export const useTodoItems = () => {
   const { value, setValue } = useTestContext();
 
-  return [
-    value.todoItems,
-    (todoItems) => setValue({ ...value, todoItems })
-  ];
-}
+  return [value.todoItems, (todoItems) => setValue({ ...value, todoItems })];
+};
