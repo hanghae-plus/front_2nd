c29570590735675b872794cdef9f0b4c4c78c6f4
JIHAN <74421125+bbbjihan@users.noreply.github.com>
2024-06-30 14:47:07 +0900

    render.js 구현 및 일부 테스트 케이스(render.test.js) 추가

--- packages/assignment-3/src/basic/__tests__/render.test.js
+++ packages/assignment-3/src/basic/__tests__/render.test.js
@@ -1,111 +1,286 @@
-import { describe, expect, test } from 'vitest'
-import { jsx, render } from '../render'
+import { describe, expect, test } from "vitest";
+import { jsx, render } from "../render";
 
-describe('render > ', () => {
-  describe('첫 번째 렌더링 테스트', () => {
-    test('한 개의 태그를 렌더링할 수 있다.', () => {
-      const App = jsx(
-        'div',
-        null,
-        'div의 children 입니다.'
-      );
+describe("render > ", () => {
+  describe("첫 번째 렌더링 테스트", () => {
+    test("한 개의 태그를 렌더링할 수 있다.", () => {
+      const App = jsx("div", null, "div의 children 입니다.");
 
-      const $root = document.createElement('div');
+      const $root = document.createElement("div");
       render($root, App);
 
       expect($root.innerHTML).toBe(`<div>div의 children 입니다.</div>`);
-    })
+    });
 
-    test('props를 추가할 수 있다.', () => {
+    test("props를 추가할 수 있다.", () => {
       const App = jsx(
-        'div',
-        { id: 'test-id', class: 'test-class' },
-        'div의 children 입니다.'
+        "div",
+        { id: "test-id", class: "test-class" },
+        "div의 children 입니다."
       );
 
-      const $root = document.createElement('div');
+      const $root = document.createElement("div");
       render($root, App);
 
-      expect($root.innerHTML).toBe(`<div id="test-id" class="test-class">div의 children 입니다.</div>`);
-    })
+      expect($root.innerHTML).toBe(
+        `<div id="test-id" class="test-class">div의 children 입니다.</div>`
+      );
+    });
 
-    test('자식 노드를 표현할 수 있다.', () => {
+    test("자식 노드를 표현할 수 있다.", () => {
       const App = jsx(
-        'div',
-        { id: 'test-id', class: 'test-class' },
-        jsx('p', null, '첫 번째 문단'),
-        jsx('p', null, '두 번째 문단'),
+        "div",
+        { id: "test-id", class: "test-class" },
+        jsx("p", null, "첫 번째 문단"),
+        jsx("p", null, "두 번째 문단")
       );
 
-      const $root = document.createElement('div');
+      const $root = document.createElement("div");
       render($root, App);
 
-      expect($root.innerHTML).toBe(`<div id="test-id" class="test-class"><p>첫 번째 문단</p><p>두 번째 문단</p></div>`);
-    })
-  })
+      expect($root.innerHTML).toBe(
+        `<div id="test-id" class="test-class"><p>첫 번째 문단</p><p>두 번째 문단</p></div>`
+      );
+    });
 
-  describe('리렌더링 테스트 - 변경된 내용만 반영되도록 한다.', () => {
-    test('하위 노드 추가', () => {
-      const $root = document.createElement('div');
+    test("세 번째 뎁스 테스트", () => {
+      const App = jsx(
+        "div",
+        { id: "test-id", class: "test-class" },
+        jsx(
+          "div",
+          { id: "1" },
+          jsx("p", null, "첫 번째 문단 첫 번째 글"),
+          jsx("p", null, "첫 번째 문단 두 번째 글")
+        ),
+        jsx(
+          "div",
+          { id: "2" },
+          jsx("p", null, "두 번째 문단 첫 번째 글"),
+          jsx("p", null, "두 번째 문단 두 번째 글")
+        )
+      );
+
+      const $root = document.createElement("div");
+      render($root, App);
+
+      expect($root.innerHTML).toBe(
+        `<div id="test-id" class="test-class"><div id="1"><p>첫 번째 문단 첫 번째 글</p><p>첫 번째 문단 두 번째 글</p></div><div id="2"><p>두 번째 문단 첫 번째 글</p><p>두 번째 문단 두 번째 글</p></div></div>`
+      );
+    });
+  });
+
+  describe("리렌더링 테스트 - 변경된 내용만 반영되도록 한다.", () => {
+    test("하위 노드 추가", () => {
+      const $root = document.createElement("div");
 
       const App = jsx(
-        'div',
-        { id: 'test-id', class: 'test-class' },
-        jsx('p', null, '첫 번째 문단'),
-        jsx('p', null, '두 번째 문단'),
+        "div",
+        { id: "test-id", class: "test-class" },
+        jsx("p", null, "첫 번째 문단"),
+        jsx("p", null, "두 번째 문단")
       );
 
       render($root, App);
 
-      expect($root.innerHTML).toBe(`<div id="test-id" class="test-class"><p>첫 번째 문단</p><p>두 번째 문단</p></div>`);
+      expect($root.innerHTML).toBe(
+        `<div id="test-id" class="test-class"><p>첫 번째 문단</p><p>두 번째 문단</p></div>`
+      );
 
-      const children = [...$root.querySelectorAll('p')];
+      const children = [...$root.querySelectorAll("p")];
 
-      render($root, jsx(
-        'div',
-        { id: 'test-id', class: 'test-class' },
-        jsx('p', null, '첫 번째 문단'),
-        jsx('p', null, '두 번째 문단'),
-        jsx('p', null, '세 번째 문단'),
-      ), App);
+      render(
+        $root,
+        jsx(
+          "div",
+          { id: "test-id", class: "test-class" },
+          jsx("p", null, "첫 번째 문단"),
+          jsx("p", null, "두 번째 문단"),
+          jsx("p", null, "세 번째 문단")
+        ),
+        App
+      );
 
-      expect($root.innerHTML).toBe(`<div id="test-id" class="test-class"><p>첫 번째 문단</p><p>두 번째 문단</p><p>세 번째 문단</p></div>`);
+      expect($root.innerHTML).toBe(
+        `<div id="test-id" class="test-class"><p>첫 번째 문단</p><p>두 번째 문단</p><p>세 번째 문단</p></div>`
+      );
 
-      const newChildren = [...$root.querySelectorAll('p')];
+      const newChildren = [...$root.querySelectorAll("p")];
 
       expect(children[0]).toBe(newChildren[0]);
       expect(children[1]).toBe(newChildren[1]);
       expect(children[2]).not.toBe(newChildren[2]);
-    })
+    });
 
-    test('props 수정', () => {
-      const $root = document.createElement('div');
+    test("props 수정", () => {
+      const $root = document.createElement("div");
       const App = jsx(
-        'div',
-        { id: 'test-id', class: 'test-class' },
-        jsx('p', null, '첫 번째 문단'),
-        jsx('p', null, '두 번째 문단'),
-      )
+        "div",
+        { id: "test-id", class: "test-class" },
+        jsx("p", null, "첫 번째 문단"),
+        jsx("p", null, "두 번째 문단")
+      );
 
       render($root, App);
 
-      expect($root.innerHTML).toBe(`<div id="test-id" class="test-class"><p>첫 번째 문단</p><p>두 번째 문단</p></div>`);
+      expect($root.innerHTML).toBe(
+        `<div id="test-id" class="test-class"><p>첫 번째 문단</p><p>두 번째 문단</p></div>`
+      );
 
-      const children = [...$root.querySelectorAll('p')];
+      const children = [...$root.querySelectorAll("p")];
 
-      render($root, jsx(
-        'div',
-        null,
-        jsx('p', null, '첫 번째 문단'),
-        jsx('p', null, '두 번째 문단'),
-      ), App);
+      render(
+        $root,
+        jsx(
+          "div",
+          null,
+          jsx("p", null, "첫 번째 문단"),
+          jsx("p", null, "두 번째 문단")
+        ),
+        App
+      );
 
-      expect($root.innerHTML).toBe(`<div><p>첫 번째 문단</p><p>두 번째 문단</p></div>`);
+      expect($root.innerHTML).toBe(
+        `<div><p>첫 번째 문단</p><p>두 번째 문단</p></div>`
+      );
 
-      const newChildren = [...$root.querySelectorAll('p')];
+      const newChildren = [...$root.querySelectorAll("p")];
 
       expect(children[0]).toBe(newChildren[0]);
       expect(children[1]).toBe(newChildren[1]);
-    })
-  })
-})
+    });
+
+    test("만약 newNode가 없고 oldNode만 있다면 parent에서 oldNode를 제거", () => {
+      const App = jsx(
+        "div",
+        { id: "test-id", class: "test-class" },
+        jsx("p", null, "첫 번째 문단"),
+        jsx("p", null, "두 번째 문단")
+      );
+
+      const $root = document.createElement("div");
+      render($root, App);
+
+      render($root, undefined, jsx("p", null, "첫 번째 문단"));
+
+      expect($root.innerHTML).toBe(
+        `<div id="test-id" class="test-class"><p>두 번째 문단</p></div>`
+      );
+    });
+
+    test("newNode와 oldNode의 타입이 다르다면 oldNode를 newNode로 교체", () => {
+      const App = jsx(
+        "div",
+        { id: "test-id", class: "test-class" },
+        jsx("p", null, "첫 번째 문단"),
+        jsx("p", null, "두 번째 문단")
+      );
+
+      const $root = document.createElement("div");
+      render($root, App);
+
+      render(
+        $root,
+        jsx("span", null, "두 번째 문단"),
+        jsx("p", null, "두 번째 문단")
+      );
+
+      expect($root.innerHTML).toBe(
+        `<div id="test-id" class="test-class"><p>첫 번째 문단</p><span>두 번째 문단</span></div>`
+      );
+    });
+
+    test("만약 newNode와 oldNode 둘 다 문자열이고 서로 다르다면 oldNode를 newNode로 교체", () => {
+      const App = jsx(
+        "div",
+        { id: "test-id", class: "test-class" },
+        jsx("p", null, "첫 번째 문단"),
+        jsx("p", null, "두 번째 문단")
+      );
+
+      const $root = document.createElement("div");
+      render($root, App);
+
+      render($root, "사실 세 번째 문단", "두 번째 문단");
+
+      expect($root.innerHTML).toBe(
+        `<div id="test-id" class="test-class"><p>첫 번째 문단</p><p>사실 세 번째 문단</p></div>`
+      );
+    });
+
+    test("세 번째 뎁스 및 newNode와 oldNode가 깊이를 가질 때 테스트", () => {
+      const App = jsx(
+        "div",
+        { id: "test-id", class: "test-class" },
+        jsx(
+          "div",
+          { id: "1" },
+          jsx("p", null, "첫 번째 문단 첫 번째 글"),
+          jsx("p", null, "첫 번째 문단 두 번째 글")
+        ),
+        jsx(
+          "div",
+          { id: "2" },
+          jsx("p", null, "두 번째 문단 첫 번째 글"),
+          jsx("p", null, "두 번째 문단 두 번째 글")
+        )
+      );
+
+      const $root = document.createElement("div");
+      render($root, App);
+
+      render(
+        $root,
+        jsx(
+          "div",
+          { id: "test-id", class: "test-class" },
+          jsx(
+            "div",
+            { id: "1" },
+            jsx("p", null, "첫 번째 문단 첫 번째 글"),
+            jsx("p", null, "첫 번째 문단 두 번째 글")
+          ),
+          jsx(
+            "div",
+            { id: "2" },
+            jsx("p", null, "두 번째 문단 첫 번째 글"),
+            jsx("p", null, "두 번째 문단 두 번째 글")
+          ),
+          jsx(
+            "div",
+            { id: "3" },
+            jsx("p", null, "세 번째 문단 첫 번째 글"),
+            jsx("p", null, "세 번째 문단 두 번째 글")
+          )
+        ),
+        App
+      );
+
+      render(
+        $root,
+        jsx(
+          "div",
+          { id: "2" },
+          jsx("p", null, "두 번째 문단 사실 세 번째 글"),
+          jsx("p", null, "두 번째 문단 두 번째 글")
+        ),
+        jsx(
+          "div",
+          { id: "2" },
+          jsx("p", null, "두 번째 문단 첫 번째 글"),
+          jsx("p", null, "두 번째 문단 두 번째 글")
+        )
+      );
+
+      render(
+        $root,
+        "두 번째 문단 사실 이게 진짜 세 번째 글",
+        "두 번째 문단 두 번째 글"
+      );
+
+      expect($root.innerHTML).toBe(
+        `<div id="test-id" class="test-class"><div id="1"><p>첫 번째 문단 첫 번째 글</p><p>첫 번째 문단 두 번째 글</p></div><div id="2"><p>두 번째 문단 사실 세 번째 글</p><p>두 번째 문단 사실 이게 진짜 세 번째 글</p></div><div id="3"><p>세 번째 문단 첫 번째 글</p><p>세 번째 문단 두 번째 글</p></div></div>`
+      );
+    });
+  });
+});
--- packages/assignment-3/src/basic/render.js
+++ packages/assignment-3/src/basic/render.js
@@ -1,44 +1,162 @@
+import { deepEquals } from "../../../assignment-2/src/basic/basic";
+import { isNill } from "./utils";
+
 export function jsx(type, props, ...children) {
-  return {}
+  return {
+    type,
+    props,
+    children,
+  };
+}
+
+function setProp(target, key, value) {
+  if (key === "class") {
+    target.classList.add(value);
+    // https://stackoverflow.com/questions/507138/how-to-add-a-class-to-a-given-element
+
+    return;
+  }
+  target[key] = value;
+}
+
+function removeProp(target, key) {
+  target.removeAttribute(key);
 }
 
 export function createElement(node) {
+  const element = document.createElement(node.type);
+
+  if (!isNill(node.props)) {
+    const propKeys = Object.keys(node.props);
+
+    for (let propKey of propKeys) {
+      setProp(element, propKey, node.props[propKey]);
+    }
+  }
+
+  for (let child of node.children) {
+    if (typeof child === "string") {
+      element.innerHTML += child;
+      continue;
+    }
+    element.appendChild(createElement(child));
+    // https://www.w3schools.com/jsref/met_node_appendchild.asp
+  }
+  return element;
   // jsx를 dom으로 변환
 }
 
 function updateAttributes(target, newProps, oldProps) {
-  // newProps들을 반복하여 각 속성과 값을 확인
-  //   만약 oldProps에 같은 속성이 있고 값이 동일하다면
-  //     다음 속성으로 넘어감 (변경 불필요)
-  //   만약 위 조건에 해당하지 않는다면 (속성값이 다르거나 구속성에 없음)
-  //     target에 해당 속성을 새 값으로 설정
+  const newKeys = isNill(newProps) ? [] : Object.keys(newProps);
+  const oldKeys = isNill(oldProps) ? [] : Object.keys(oldProps);
 
-  // oldProps을 반복하여 각 속성 확인
-  //   만약 newProps들에 해당 속성이 존재한다면
-  //     다음 속성으로 넘어감 (속성 유지 필요)
-  //   만약 newProps들에 해당 속성이 존재하지 않는다면
-  //     target에서 해당 속성을 제거
+  for (let newKey of newKeys) {
+    if (
+      isNill(oldProps) ||
+      isNill(oldProps[newKey]) ||
+      !deepEquals(newProps[newKey], oldProps[newKey])
+    ) {
+      setProp(target, newKey, newProps[newKeys]);
+    }
+  }
+  for (let oldKey of oldKeys) {
+    if (isNill(newProps) || isNill(newProps[oldKey])) {
+      removeProp(target, oldKey);
+    }
+  }
 }
 
 export function render(parent, newNode, oldNode, index = 0) {
-  // 1. 만약 newNode가 없고 oldNode만 있다면
-  //   parent에서 oldNode를 제거
-  //   종료
+  if (isNill(newNode) && !isNill(oldNode)) {
+    // console.log(
+    //   "만약 newNode가 없고 oldNode만 있다면 parent에서 oldNode를 제거"
+    // );
+    const oldElement = createElement(oldNode);
 
-  // 2. 만약 newNode가 있고 oldNode가 없다면
-  //   newNode를 생성하여 parent에 추가
-  //   종료
+    if (oldElement.outerHTML === parent.outerHTML) {
+      parent.remove();
+    }
 
-  // 3. 만약 newNode와 oldNode 둘 다 문자열이고 서로 다르다면
-  //   oldNode를 newNode로 교체
-  //   종료
+    for (const child of parent.children) {
+      render(child, newNode, oldNode);
+    }
 
-  // 4. 만약 newNode와 oldNode의 타입이 다르다면
-  //   oldNode를 newNode로 교체
-  //   종료
+    // TODO: parent에 있는 모든 oldNode를 제거할 건지, 하나만 제거하고 멈출건지 확정(지금은 모든 노드 제거)
+    return;
+  }
 
-  // 5. newNode와 oldNode에 대해 updateAttributes 실행
+  if (!isNill(newNode) && isNill(oldNode)) {
+    // console.log(
+    //   "만약 newNode가 있고 oldNode가 없다면 newNode를 생성하여 parent에 추가"
+    // );
+    const newElement = createElement(newNode);
 
-  // 6. newNode와 oldNode 자식노드들 중 더 긴 길이를 가진 것을 기준으로 반복
-  //   각 자식노드에 대해 재귀적으로 render 함수 호출
+    if (isNill(parent)) {
+      parent = newElement;
+    } else {
+      parent.append(newElement);
+    }
+
+    return;
+  }
+
+  if (
+    typeof newNode === "string" &&
+    typeof oldNode === "string" &&
+    newNode !== oldNode
+  ) {
+    // console.log(
+    //   "만약 newNode와 oldNode 둘 다 문자열이고 서로 다르다면 oldNode를 newNode로 교체"
+    // );
+    if (parent.innerHTML === oldNode) {
+      parent.innerHTML = newNode;
+    }
+
+    for (const child of parent.children) {
+      render(child, newNode, oldNode);
+    }
+    return;
+  }
+
+  if (newNode.type !== oldNode.type) {
+    // console.log("newNode와 oldNode의 타입이 다르다면 oldNode를 newNode로 교체");
+    const oldElement = createElement(oldNode);
+    // TODO: 모든 재귀 호출에서 계속 oldElement 생성하는 중
+    // TODO: 근데 타입끼리 비교하려면 맨 처음에 생성하고 계속 가지고 내려가기 힘들 듯?
+
+    if (oldElement.outerHTML === parent.outerHTML) {
+      const newElement = createElement(newNode);
+      const grandParent = parent.parentNode;
+      grandParent.replaceChild(newElement, parent);
+      // https://developer.mozilla.org/ko/docs/Web/API/Node/replaceChild
+    }
+
+    for (const child of parent.children) {
+      render(child, newNode, oldNode);
+    }
+
+    // TODO: parent에 있는 모든 oldNode를 변경할 건지, 하나만 변경하고 멈출건지 확정(지금은 모든 노드 변경)
+    return;
+  }
+
+  if (deepEquals(newNode, oldNode)) {
+    return;
+  }
+
+  // console.log("newNode와 oldNode에 대해 updateAttributes 실행");
+  updateAttributes(parent.children[0], newNode.props, oldNode.props);
+
+  // console.log(
+  //   "newNode와 oldNode 자식노드들 중 더 긴 길이를 가진 것을 기준으로 반복, 자식노드 재귀호출"
+  // );
+  const maxChildrenCnt =
+    newNode.children.length > oldNode.children.length
+      ? newNode.children.length
+      : oldNode.children.length;
+
+  for (let i = 0; i < maxChildrenCnt; i++) {
+    for (const child of parent.children) {
+      render(child, newNode.children[i], oldNode.children[i]);
+    }
+  }
 }
