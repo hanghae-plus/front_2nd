6b0450fad0e41d5d21f8744308565e0d86c10a78
JIHAN <74421125+bbbjihan@users.noreply.github.com>
2024-06-30 20:04:01 +0900

    hooks 구현

--- packages/assignment-3/src/basic/hooks.js
+++ packages/assignment-3/src/basic/hooks.js
@@ -1,15 +1,49 @@
+import { deepEquals } from "../../../assignment-2/src/basic/basic";
+
 export function createHooks(callback) {
+  let states = [];
+  let statesCurrentIndex = -1;
+
   const useState = (initState) => {
-    return [];
+    states.push(initState);
+    statesCurrentIndex += 1;
+    const currentIndex = statesCurrentIndex;
+
+    const state = states[currentIndex];
+
+    const setState = function (newValue) {
+      if (!deepEquals(states[currentIndex], newValue)) {
+        states[currentIndex] = newValue;
+        callback();
+      }
+    };
+
+    return [state, setState];
   };
 
+  let memoValues = [];
+  let memoRefs = [];
+  let memosCurrentIndex = -1;
+
   const useMemo = (fn, refs) => {
-    return fn();
+    memoValues.push(fn());
+    memoRefs.push(refs);
+    memosCurrentIndex += 1;
+    const currentIndex = memosCurrentIndex;
+
+    if (!deepEquals(memoRefs[currentIndex], refs)) {
+      memoRefs[currentIndex] = refs;
+      memoValues[currentIndex] = fn();
+    }
+
+    const value = memoValues[currentIndex];
+    return value;
   };
 
   const resetContext = () => {
-
-  }
+    statesCurrentIndex = -1;
+    memosCurrentIndex = -1;
+  };
 
   return { useState, useMemo, resetContext };
 }
