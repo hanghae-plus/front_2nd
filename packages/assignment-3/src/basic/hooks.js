d403edf22347a1e94620f11960a33efd4a33b729
hyesung99 <4538ase@gamil.com>
2024-06-30 15:43:25 +0900

    feat : useState 테스트 통과

--- packages/assignment-3/src/basic/hooks.js
+++ packages/assignment-3/src/basic/hooks.js
@@ -1,6 +1,22 @@
 export function createHooks(callback) {
+  let state = [];
+  let currentStateIndex = 0;
   const useState = (initState) => {
-    return [];
+    let currentIndex = currentStateIndex;
+    currentStateIndex++;
+
+    if (state[currentIndex] === undefined) {
+      state[currentIndex] = initState;
+    }
+
+    const setState = (newState) => {
+      if (state[currentIndex] !== newState) {
+        state[currentIndex] = newState;
+        callback();
+      }
+    };
+
+    return [state[currentIndex], setState];
   };
 
   const useMemo = (fn, refs) => {
@@ -8,8 +24,8 @@ export function createHooks(callback) {
   };
 
   const resetContext = () => {
-
-  }
+    currentStateIndex = 0;
+  };
 
   return { useState, useMemo, resetContext };
 }
