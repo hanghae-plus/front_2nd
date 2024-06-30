447898785192528d3c8304eb014097a463ae3187
hyesung99 <4538ase@gamil.com>
2024-06-30 22:31:41 +0900

    feat : render 테스트 통과

--- packages/assignment-3/src/basic/render.js
+++ packages/assignment-3/src/basic/render.js
@@ -1,9 +1,28 @@
 export function jsx(type, props, ...children) {
-  return {}
+  return { type, props, children };
 }
 
 export function createElement(node) {
   // jsx를 dom으로 변환
+  const { type, props, children } = node;
+  const element = document.createElement(type);
+
+  if (props) {
+    Object.entries(props).forEach(([key, value]) => {
+      element.setAttribute(key, value);
+    });
+  }
+
+  if (children) {
+    children.forEach((child) => {
+      if (typeof child === "string") {
+        element.appendChild(document.createTextNode(child));
+      } else {
+        element.appendChild(createElement(child));
+      }
+    });
+  }
+  return element;
 }
 
 function updateAttributes(target, newProps, oldProps) {
@@ -12,33 +31,73 @@ function updateAttributes(target, newProps, oldProps) {
   //     다음 속성으로 넘어감 (변경 불필요)
   //   만약 위 조건에 해당하지 않는다면 (속성값이 다르거나 구속성에 없음)
   //     target에 해당 속성을 새 값으로 설정
-
+  for (const [key, value] of Object.entries(newProps || {})) {
+    if (oldProps[key] !== value) {
+      target.setAttribute(key, value);
+    }
+  }
   // oldProps을 반복하여 각 속성 확인
   //   만약 newProps들에 해당 속성이 존재한다면
   //     다음 속성으로 넘어감 (속성 유지 필요)
   //   만약 newProps들에 해당 속성이 존재하지 않는다면
   //     target에서 해당 속성을 제거
+  for (const key of Object.keys(oldProps || {})) {
+    if (!Object.keys(newProps || {}).includes(key)) {
+      target.removeAttribute(key);
+    }
+  }
 }
 
 export function render(parent, newNode, oldNode, index = 0) {
   // 1. 만약 newNode가 없고 oldNode만 있다면
   //   parent에서 oldNode를 제거
   //   종료
-
+  if (!newNode && oldNode) {
+    parent.removeChild(parent.childNodes[index]);
+    return;
+  }
   // 2. 만약 newNode가 있고 oldNode가 없다면
   //   newNode를 생성하여 parent에 추가
   //   종료
-
+  if (newNode && !oldNode) {
+    parent.appendChild(createElement(newNode));
+    return;
+  }
   // 3. 만약 newNode와 oldNode 둘 다 문자열이고 서로 다르다면
   //   oldNode를 newNode로 교체
   //   종료
-
+  if (
+    typeof newNode === "string" &&
+    typeof oldNode === "string" &&
+    newNode !== oldNode
+  ) {
+    parent.replaceChild(
+      document.createTextNode(newNode),
+      parent.childNodes[index]
+    );
+    return;
+  }
   // 4. 만약 newNode와 oldNode의 타입이 다르다면
   //   oldNode를 newNode로 교체
   //   종료
-
+  if (newNode.type !== oldNode.type) {
+    parent.replaceChild(createElement(newNode), parent.childNodes[index]);
+    return;
+  }
   // 5. newNode와 oldNode에 대해 updateAttributes 실행
-
+  updateAttributes(parent.childNodes[index], newNode.props, oldNode.props);
   // 6. newNode와 oldNode 자식노드들 중 더 긴 길이를 가진 것을 기준으로 반복
   //   각 자식노드에 대해 재귀적으로 render 함수 호출
+  for (
+    let i = 0;
+    i < Math.max(newNode?.children?.length, oldNode?.children?.length);
+    i++
+  ) {
+    render(
+      parent.childNodes[index],
+      newNode.children[i],
+      oldNode.children[i],
+      i
+    );
+  }
 }
