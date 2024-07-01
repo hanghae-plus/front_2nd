3c149d4b228cc55a1ecbb9d0bcac99b5866c2f80
2pandi <coder.2pandi@gmail.com>
2024-07-01 22:15:20 +0900

    render.js 테스트 통과

--- packages/assignment-3/src/basic/render.js
+++ packages/assignment-3/src/basic/render.js
@@ -1,5 +1,7 @@
 // @ts-check
 
+import { deepEquals } from "../../../assignment-2/src/basic/basic";
+
 /**
  * jsx
  * @typedef {Object} jsx
@@ -9,7 +11,12 @@
  */
 
 /**
- *
+ * node
+ * @typedef {jsx | string} node
+ */
+
+/**
+ * jsx를 생성합니다.
  * @param {jsx['type']} type - node name
  * @param {jsx['props']} props
  * @param {string[] | jsx[]} children
@@ -24,8 +31,8 @@ export function jsx(type, props = null, ...children) {
 }
 
 /**
- *
- * @param {string | jsx} node
+ * node 정보를 받아 HTMLElement를 만듭니다.
+ * @param {node} node
  * @returns {HTMLElement}
  */
 export function createElement(node) {
@@ -33,23 +40,55 @@ export function createElement(node) {
   if (typeof node === "string") {
     return document.createElement(node);
   }
+
   const dom = document.createElement(node.type);
-  if (node.props) {
-    if (node.props.id) dom.setAttribute("id", node.props.id);
-    if (node.props.class) dom.setAttribute("class", node.props.class);
-  }
-  if (typeof node.children[0] === "string") {
-    dom.innerHTML = node.children[0];
-  } else {
-    node.children.forEach((jsx) => {
-      dom.appendChild(createElement(jsx));
+
+  if (node.props !== null) {
+    Object.keys(node.props).forEach((key) => {
+      dom.setAttribute(key, node.props[key]);
     });
+  }
+
+  if (node.children?.length >= 0) {
+    if (typeof node.children[0] === "string") {
+      dom.innerHTML = node.children[0];
+    } else {
+      node.children.forEach((jsx) => {
+        dom.appendChild(createElement(jsx));
+      });
+    }
   }
 
   return dom;
 }
 
+/**
+ * oldProps와 newProps를 비교하여 변경점을 반영합니다.
+ * @param {HTMLElement} target
+ * @param {jsx['props']} newProps
+ * @param {jsx['props']} oldProps
+ */
 function updateAttributes(target, newProps, oldProps) {
+  if (oldProps == null || target == null) return;
+
+  if (newProps == null) {
+    target.removeAttribute("id");
+    target.removeAttribute("class");
+    return;
+  }
+
+  for (const key in newProps) {
+    if (newProps[key] !== oldProps[key]) {
+      target.setAttribute(key, newProps[key]);
+    }
+  }
+
+  for (const key in oldProps) {
+    if (!(key in newProps)) {
+      target.removeAttribute(key);
+    }
+  }
+
   // newProps들을 반복하여 각 속성과 값을 확인
   //   만약 oldProps에 같은 속성이 있고 값이 동일하다면
   //     다음 속성으로 넘어감 (변경 불필요)
@@ -62,14 +101,85 @@ function updateAttributes(target, newProps, oldProps) {
   //     target에서 해당 속성을 제거
 }
 
-export function render(parent, newNode, oldNode, index = 0) {
-  // const parentJsx = createElement(parent);
-  if (!newNode && oldNode) {
-    console.log("case1");
+/**
+ * oldNode와 newNode 중 긴 node를 기준으로 동일한 child가 있는지 확인하고 update 합니다.
+ * @param {Node} parent
+ * @param {jsx} newNode
+ * @param {jsx} oldNode
+ */
+function updateNodes(parent, newNode, oldNode) {
+  if (parent == null) return;
+  const longerNode =
+    newNode.children.length >= oldNode.children.length ? newNode : oldNode;
+  const shorterNode = longerNode === newNode ? oldNode : newNode;
+
+  const equalIdx = [];
+  longerNode.children.forEach((child, i) => {
+    if (deepEquals(child, shorterNode.children[i])) equalIdx.push(i);
+  });
+  const oldLength = parent.firstChild?.childNodes.length;
+  if (oldLength === equalIdx.length) {
+    longerNode.children.forEach((child, i) => {
+      if (i >= oldLength) parent.firstChild?.appendChild(createElement(child));
+    });
   }
+}
+
+/**
+ *
+ * @param {Node} parent
+ * @param {node} newNode
+ * @param {node} oldNode
+ * @param {number} index
+ */
+export function render(parent, newNode, oldNode, index = 0) {
+  if (parent == null) return;
+
+  // case1
+  if (newNode == null && oldNode) {
+    const child = parent.firstChild;
+    if (child) parent.removeChild(child);
+    return;
+  }
+
+  // case2
   if (newNode && oldNode == null) {
     parent.appendChild(createElement(newNode));
+    return;
   }
+
+  // case3, case4
+  if (
+    (typeof newNode === "string" &&
+      typeof oldNode === "string" &&
+      newNode !== oldNode) ||
+    (typeof newNode !== "string" &&
+      typeof oldNode !== "string" &&
+      newNode.type !== oldNode.type)
+  ) {
+    const child = parent.firstChild;
+    if (child) parent.replaceChild(child, createElement(newNode));
+    return;
+  }
+
+  // case5, case6
+  if (
+    typeof newNode !== "string" &&
+    typeof oldNode !== "string" &&
+    newNode.children.length > 0 &&
+    oldNode.children.length > 0
+  ) {
+    updateNodes(parent, newNode, oldNode);
+    updateAttributes(parent.childNodes[index], newNode.props, oldNode.props);
+    const max = Math.max(newNode.children.length, oldNode.children.length);
+    for (let i = 0; i < max; i++) {
+      render(parent.childNodes[i], newNode.children[i], oldNode.children[i], i);
+    }
+  }
+  updateAttributes(parent.childNodes[index], newNode.props, oldNode.props);
+
+  //   }
+
   // 1. 만약 newNode가 없고 oldNode만 있다면
   //   parent에서 oldNode를 제거
   //   종료
