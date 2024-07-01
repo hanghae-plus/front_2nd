4af60cb7c9db6c7dfebfcdf0dc380c6590276e8a
2pandi <coder.2pandi@gmail.com>
2024-07-01 23:21:56 +0900

    useState 테스트 통과

--- packages/assignment-3/src/basic/hooks.js
+++ packages/assignment-3/src/basic/hooks.js
@@ -1,6 +1,22 @@
+import { shallowEquals } from "../../../assignment-2/src/basic/basic";
+
 export function createHooks(callback) {
+  let states = [];
+  let key = 0;
   const useState = (initState) => {
-    return [];
+    const currentKey = key;
+    if (currentKey === states.length) {
+      states[currentKey] = initState;
+    }
+
+    key++;
+
+    const setState = (newState) => {
+      if (shallowEquals(states[currentKey], newState)) return;
+      states[currentKey] = newState;
+      callback();
+    };
+    return [states[currentKey], setState];
   };
 
   const useMemo = (fn, refs) => {
@@ -8,8 +24,8 @@ export function createHooks(callback) {
   };
 
   const resetContext = () => {
-
-  }
+    key = 0;
+  };
 
   return { useState, useMemo, resetContext };
 }
