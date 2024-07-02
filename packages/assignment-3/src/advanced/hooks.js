b0d4073bb68861eb262edce596762f3259854b75
seojunhwan <seojunhwan@kakao.com>
2024-07-02 23:29:19 +0900

    feat: advanced useState

--- packages/assignment-3/src/advanced/hooks.js
+++ packages/assignment-3/src/advanced/hooks.js
@@ -1,15 +1,33 @@
 export function createHooks(callback) {
+  const states = [];
+  let currentStateId = 0;
+
   const useState = (initState) => {
-    return [];
+    const stateId = currentStateId;
+    currentStateId++;
+
+    if (states[stateId] === undefined) {
+      states[stateId] = initState;
+    }
+
+    const setState = (newState) => {
+      if (states[stateId] === newState) return;
+      states[stateId] = newState;
+
+      // 상태 변경을 다음 애니메이션 프레임에 동기화하여 UI 업데이트
+      requestAnimationFrame(() => {
+        callback();
+      });
+    };
+
+    return [states[stateId], setState];
   };
 
   const useMemo = (fn, refs) => {
     return fn();
   };
 
-  const resetContext = () => {
-
-  }
+  const resetContext = () => {};
 
   return { useState, useMemo, resetContext };
 }
