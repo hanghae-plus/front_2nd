94a5325b9b13699d52e669485dbc86b152d6b2bf
metacode22 <alohajune22@gmail.com>
2024-07-04 00:02:56 +0900

    useState 구현

--- packages/assignment-3/src/basic/hooks.js
+++ packages/assignment-3/src/basic/hooks.js
@@ -1,6 +1,34 @@
 export function createHooks(callback) {
+  let currentIndex = 0;
+  const hooks = [];
+
   const useState = (initState) => {
-    return [];
+    if (hooks[currentIndex] == null) {
+      if (typeof initState === 'function') {
+        hooks[currentIndex] = initState();
+      } else {
+        hooks[currentIndex] = initState;
+      }
+    }
+
+    const state = hooks[currentIndex];
+    currentIndex++;
+
+    function setState(nextState) {
+      if (typeof nextState === 'function') {
+        nextState(state);
+        callback();
+        resetContext();
+      }
+
+      if (!Object.is(nextState, state)) {
+        hooks[currentIndex] = nextState;
+        callback();
+        resetContext();
+      }
+    }
+
+    return [state, setState];
   };
 
   const useMemo = (fn, refs) => {
@@ -8,8 +36,8 @@ export function createHooks(callback) {
   };
 
   const resetContext = () => {
-
-  }
+    currentIndex = 0;
+  };
 
   return { useState, useMemo, resetContext };
 }
