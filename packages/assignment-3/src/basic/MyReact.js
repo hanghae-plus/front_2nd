d887fc832888390dc3ce65cd49d439180ea1c998
Joie-Kim <joie.huiju@gmail.com>
2024-07-04 00:14:15 +0900

    [3주차] 기본과제 - MyReact(1) 렌더링 테스트

--- packages/assignment-3/src/basic/MyReact.js
+++ packages/assignment-3/src/basic/MyReact.js
@@ -1,11 +1,25 @@
-import { createHooks } from "./hooks";
-import { render as updateElement } from "./render";
+import { createHooks } from './hooks';
+import { render as updateElement } from './render';
 
 function MyReact() {
   const _render = () => {};
-  function render($root, rootComponent) {}
 
-  const { useState, useMemo, resetContext: resetHookContext } = createHooks(_render);
+  /**
+   * 리액트 렌더 함수
+   *
+   * @param {*} $root 요소를 넣을 상위 노드
+   * @param {Function} rootComponent 요소 반환 함수
+   */
+  function render($root, rootComponent) {
+    const component = rootComponent();
+    updateElement($root, component);
+  }
+
+  const {
+    useState,
+    useMemo,
+    resetContext: resetHookContext,
+  } = createHooks(_render);
 
   return { render, useState, useMemo };
 }
