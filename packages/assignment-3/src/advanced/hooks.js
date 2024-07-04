1e00a9e07b78e68acd56e1370c124b46d23f8ba5
barleymilk <barleymilk640@gmail.com>
2024-07-04 22:41:27 +0900

    feat: createHooks 함수 추가

--- packages/assignment-3/src/advanced/hooks.js
+++ packages/assignment-3/src/advanced/hooks.js
@@ -1,6 +1,46 @@
 export function createHooks(callback) {
+  let state = []; // 상태를 저장
+  let setters = []; // 상태를 변경하는 setter 함수들을 저장
+  let currentIdx = 0; // 현재 상태의 인덱스
+  let pendingState = []; // 다음 상태를 저장
+  let isFramePending = false; // requestAnimationFrame 호출 여부
+
+  const runCallback = () => {
+    currentIdx = 0;
+    isFramePending = false;
+    callback();
+  };
+
   const useState = (initState) => {
-    return [];
+    const idx = currentIdx;
+
+    if (state.length === idx) {
+      state.push(initState);
+      pendingState.push(initState);
+    }
+
+    const setState = (newState) => {
+      pendingState[idx] = newState;
+
+      // 프레임 대기 상태가 아니면 requestAnimationFrame 호출
+      if (!isFramePending) {
+        isFramePending = true;
+        requestAnimationFrame(() => {
+          for (let i = 0; i < pendingState.length; i++) {
+            state[i] = pendingState[i]
+          }
+          runCallback();
+        });
+
+      }
+    };
+
+    if (setters.length === idx) {
+      setters.push(setState);
+    }
+
+    currentIdx++;
+    return [state[idx], setters[idx]];
   };
 
   const useMemo = (fn, refs) => {
