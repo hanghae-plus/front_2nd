8f6b6a278b716aa33865c3546709fa4433b79e5f
cho-subin <100771469+cho-subin@users.noreply.github.com>
2024-07-05 02:11:28 +0900

    render 수정

--- packages/assignment-3/src/basic/MyReact.js
+++ packages/assignment-3/src/basic/MyReact.js
@@ -2,8 +2,25 @@ import { createHooks } from "./hooks";
 import { render as updateElement } from "./render";
 
 function MyReact() {
-  const _render = () => {};
-  function render($root, rootComponent) {}
+  let currentRoot = null;
+  let currentComponent = null;
+  let oldComponent = null;
+
+  const _render = () => {
+    resetHookContext();
+    const newElement = currentComponent();
+    updateElement(currentRoot, newElement, oldComponent);
+    oldComponent = newElement;
+  };
+
+  function render($root, rootComponent) {
+    resetHookContext();
+    currentRoot = $root;
+    currentComponent = rootComponent;
+    const newElement = currentComponent();
+    updateElement(currentRoot, newElement);
+    oldComponent = newElement;
+  }
 
   const { useState, useMemo, resetContext: resetHookContext } = createHooks(_render);
 
--- packages/assignment-3/src/basic/render.js
+++ packages/assignment-3/src/basic/render.js
@@ -1,4 +1,4 @@
-/** jsx는 XML처럼 생긴 구문을 JS로 변환하는 문법.
+/** 가상DOM 객체 생성 역할 : jsx는 XML처럼 생긴 구문을 JS로 변환하는 문법.
  * 그래서 jsx 함수는 props로 들어오는 구문들을 해석해서 JS객체를 리턴해야 한다.
  * type : 생성하는 요소의 유형 ('div', 'span' ....)
  * props : 요소의 속성(class, id)
@@ -18,8 +18,9 @@ export function jsx(type, props, ...children) {
   return { type, props }
 }
  
-/** props로 들어오는 node를 기반으로 JavaScript 객체로부터 DOM 요소로 변환하는 역할
+/** props로 들어오는 node를 기반으로 JavaScript 객체(가상돔)로부터 실제 DOM 요소로 변환하는 역할
  * 위의 jsx함수의 객체를 해석할때 사용.
+ * 그래서 createElement 함수의 인자로 위의 함수에서 만들어진 가상DOM 객체가 들어간다.
  */
 export function createElement(node) {
   if(typeof node === 'string'){
@@ -52,6 +53,15 @@ export function createElement(node) {
   return element;
 }
 
+/** newNode와 oldNode의 속성을 비교해서 parent.childNodes[index]
+ * updateAttributes 함수의 목적 :
+ *  1. 비교 시작 : diff 알고리즘의 기본 로직은 두 트리의 루트 노드부터 비교
+ *  2. 노드 타입 : 노드의 타입이 다르면 변경된 것으로 간주하고 전체 노드를 교체
+ *  3. 노드 값 비교 : 노드가 같은 타입이라면 값을 비교해서 다르면 변경된 것으로 간주하고 값을 교체
+ *  4. 속성 비교 : 두 노드가 객체이면서 같은 타입일때 속성을 비교.
+ *  5. 자식 노드 비교 : 두 노드의 자식 노드를 재귀적으로 비교.(길이가 다르면 긴쪽을 기준으로 비교)
+ *  위의 diff 알고리즘 흐름에서 4번을 실행하기 위한 함수.
+ */
 function updateAttributes(target, newProps, oldProps) {
   // newProps들을 반복하여 각 속성과 값을 확인
   //   만약 oldProps에 같은 속성이 있고 값이 동일하다면
@@ -78,6 +88,7 @@ function updateAttributes(target, newProps, oldProps) {
   })
 }
 
+/** diff 알고리즘을 이용해 newNode와 oldNode 두 트리구조를 비교해서 변경된 부분만 업데이트 */
 export function render(parent, newNode, oldNode, index = 0) {
   // 1. 만약 newNode가 없고 oldNode만 있다면
   //   parent에서 oldNode를 제거
@@ -118,10 +129,11 @@ export function render(parent, newNode, oldNode, index = 0) {
 
   // 6. newNode와 oldNode 자식노드들 중 더 긴 길이를 가진 것을 기준으로 반복
   //   각 자식노드에 대해 재귀적으로 render 함수 호출
-  const newNodeLength = newNode.props.children ? newNode.props.children.length : 0;
-  const oldNodeLength = oldNode.props.children ? oldNode.props.children.length : 0;
+  const newNodeChildren = Array.isArray(newNode.props.children) ? newNode.props.children : [newNode.props.children];
+  const oldNodeChildren = Array.isArray(oldNode.props.children) ? oldNode.props.children : [oldNode.props.children];
+  const maxLength = Math.max(newNodeChildren.length, oldNodeChildren.length);
 
-  for(let i=0; i<newNodeLength || i<oldNodeLength; i++){
-    render(parent.childNodes[index], newNode.props.children[i], oldNode.props.children[i], i);
+  for (let i = 0; i < maxLength; i++) {
+    render(parent.childNodes[index], newNodeChildren[i], oldNodeChildren[i], i);
   }
 }
