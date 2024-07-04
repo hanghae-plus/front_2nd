e029d566aa24bf7d1ec201fbbc882f7ca5f4e092
ChangigJeon <wjs1724@naver.com>
2024-07-04 17:55:57 +0900

    feat 3주차 advanced 문제 재구현

--- packages/assignment-3/src/advanced/hooks.js
+++ packages/assignment-3/src/advanced/hooks.js
@@ -6,9 +6,12 @@ export function createHooks(callback) {
 
   /**
    * @NOTE
-   * requestAnimation : window에서 사용가능한 메소드
-   * 페인팅할 준비가 되었을 때 지연 & 블로킹 현상 없이 부드럽게 애니메이션을 실행
-   * 주사율에 맞게 1프레임에 한번 호출된다.
+   * requestAnimation : window에서 제공하는 메소드
+   * 리페인팅하기 전에 브라우저가 호출할 인수로 콜백을 받는다.
+   * 브라우저에서 탭 이동시, 백그라운드 탭이 되는데 배터리 성능을 위해 requestAnimation은 중단된다.
+   * iframe이 display: none, visibility: hidden으로 숨겨질때도 실행 멈춤.
+   * 주사율에 맞게 1프레임에 한번 호출된다.(60Hz => 1초 60번 호출)
+   *
    * 1.작업을 batch 처리할 callStack에 변경 값을 push하고
    * 2.1프레임에 한번씩 callStack에 작업 내용이 쌓여있는지 확인후
    * 3.작업 내용이 있다면 마지막 변경사항만 반영하고, reRender()를 호출한다.
@@ -17,16 +20,18 @@ export function createHooks(callback) {
    */
   const useState = (initState) => {
     let stateIdx = currentHookIdx;
+    let callStack = [];
+
     hooks[stateIdx] = hooks[stateIdx] || initState;
 
     const state =
       typeof hooks[stateIdx] === "object" ? Object.freeze(hooks[stateIdx]) : hooks[stateIdx];
 
-    let callStack = [];
     function setState(updatedState) {
       if (hooks[stateIdx] === updatedState) return;
-      updatedState;
+
       callStack.push(updatedState);
+
       requestAnimationFrame(() => {
         if (callStack.length > 0) {
           hooks[stateIdx] = callStack.pop();
