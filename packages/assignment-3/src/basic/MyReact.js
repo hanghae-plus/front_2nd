ef1b69994c681f5621a350b8b9283186f85a9ec7
seojunhwan <seojunhwan@kakao.com>
2024-07-04 23:28:46 +0900

    docs: MyReact.js 파일에 상세 주석 추가
    - MyReact.js 파일의 각 라인에 설명 주석 추가
    - 코드의 동작과 목적을 명확히 설명하여 가독성 향상
    - 주요 함수와 변수의 역할 설명

--- packages/assignment-3/src/basic/MyReact.js
+++ packages/assignment-3/src/basic/MyReact.js
@@ -8,18 +8,25 @@ function MyReact() {
 
   const _render = () => {
     if (!root || !renderComponent) return;
+    // hook의 실행 순서에 따라 값을 참조하기 때문에 내부 참조 id를 초기화한다.
     resetHookContext();
 
+    // 새로운 노드를 렌더링한다.
     const newNode = renderComponent();
 
+    // 새로운 노드를 기존 노드와 비교하여 업데이트한다.
     updateElement(root, newNode, oldNode);
 
+    // 새로은 노드를 oldNode에 저장하여 다음 렌더링 시 비교할 수 있도록 한다.
     oldNode = newNode;
   };
 
   function render($root, component) {
+    // 새로운 노드를 렌더링하기 전에 기존 노드를 초기화한다.
     oldNode = null;
+    // 루트 노드와 컴포넌트를 저장한다.
     root = $root;
+    // 컴포넌트를 저장한다.
     renderComponent = component;
     _render();
   }
