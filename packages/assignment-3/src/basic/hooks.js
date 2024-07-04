f0a935daf9de4baadb8142f6645f2c7f447fa98b
Joie-Kim <joie.huiju@gmail.com>
2024-07-04 22:46:54 +0900

    [3주차] 기본과제 - hooks(2): useMemo

--- packages/assignment-3/src/basic/hooks.js
+++ packages/assignment-3/src/basic/hooks.js
@@ -1,53 +1,79 @@
-import { deepEquals } from "../../../assignment-2/src/basic/basic";
+import { deepEquals } from '../../../assignment-2/src/basic/basic';
 
 /**
  * 훅 생성 함수
- * 
- * @param {Function} callback 여기서는 렌더 함수
- * @returns 
- * 
- * - 상태값을 관리하는 배열 stats는 인덱스로 접근
+ *
+ * @param {Function} callback
+ * @returns
+ *
+ * - 상태값을 관리하는 배열 stats는 인덱스(key)로 접근
  * - 한 컴포넌트에서 useState를 여러 개 사용할 경우, 배열에 순서대로 쌓임
  * > 예를 들어, 아래와 같이 있다면...
  *    const [size, setSize] = react.useState(1);
  *    const [text, setText] = react.useState('hello');
  *    ==> states: [1, 'hello']
- * - 따라서 렌더링 시, 값을 찾아가는 인덱스를 초기화 해줘야 함
+ *    ==> state: 1 > key: 2 / state: 'hello' > key: 1
+ * - 따라서 렌더링 시, 값을 찾아가는 인덱스(key)를 초기화 해줘야 함
  * > 이 역할을 하는 함수가 resetContext()
  */
 export function createHooks(callback) {
+  /** useState에서 쓰이는 상태 배열 */
   let states = [];
-  let currentIndex = 0;
+  /** useMemo에서 쓰이는 상태 배열 */
+  let memos = [];
+  /** useState 상태 배열 접근 키 */
+  let currentStateKey = 0;
+  /** useMemo 상태 배열 접근 키 */
+  let currentMemoKey = 0;
+
+  //
 
   const useState = (initState) => {
-    const stateIndex = currentIndex;
-    currentIndex += 1;
+    const stateKey = currentStateKey;
+    currentStateKey += 1;
 
-    if (states[stateIndex] === undefined) {
-      states[stateIndex] = initState;
+    if (states[stateKey] === undefined) {
+      states[stateKey] = initState;
     }
 
     function setState(newValue) {
-      console.log('new Value : ', newValue);
-
-      if (!deepEquals(states[stateIndex], newValue)) {
-        console.log('here??');
-        states[stateIndex] = newValue;
-        console.log ('state : ', states);
+      if (!deepEquals(states[stateKey], newValue)) {
+        states[stateKey] = newValue;
         callback(); // 얘가 렌더 함수
       }
     }
 
-    return [states[stateIndex], setState];
+    return [states[stateKey], setState];
   };
+
+  //
 
   const useMemo = (fn, refs) => {
-    return fn();
+    const memoKey = currentMemoKey;
+    currentMemoKey += 1;
+
+    if (memos[memoKey] === undefined) {
+      memos[memoKey] = { value: fn(), refs };
+    } else {
+      const prevRefs = memos[memoKey].refs;
+      const isChanged = refs.some((newRef, i) => newRef !== prevRefs[i]);
+
+      // 의존성 배열이 변했는지 확인
+      if (isChanged) {
+        // 변했다면 값을 변경
+        memos[memoKey] = { value: fn(), refs };
+      }
+    }
+
+    return memos[memoKey].value;
   };
 
+  //
+
   const resetContext = () => {
-    currentIndex = 0;
-  }
+    currentStateKey = 0;
+    currentMemoKey = 0;
+  };
 
   return { useState, useMemo, resetContext };
 }
