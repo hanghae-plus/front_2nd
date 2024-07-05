3cb0d922bd3f47a035ae5d68ed77ff138d186158
Rachel <borapark13@gmail.com>
2024-07-05 23:33:42 +0900

    feat: resetContext 추가 및 useState 로직 변경

--- packages/assignment-3/src/basic/hooks.js
+++ packages/assignment-3/src/basic/hooks.js
@@ -1,22 +1,30 @@
 export function createHooks(callback) {
   //useState가 호출될 때마다 초기 상태값 저장
-  const _initialState = []; //초기 상태값 저장
+  let _initialState = []; //초기 상태값 저장
   //현재 상태값 별도 저장 - setState에 의해서 변경됨
-  const _currentState = []; //현재 상태값 저장
+  let _currentState = []; //현재 상태값 저장
+  let _index =0;
 
   const useState = (initState) => {
-    // 초기 상태값을 저장하고, 현재 상태값을 관리
-    const state = initState;
-    _initialState.push(state);
-    const setState = (newState) => {
-      if (state === newState) {
-        //값이 동일하면 렌더링을 하지 않음
-        return;
-      }
+    const currentIndex = _index;
 
+    if (_initialState.length <= currentIndex) {
+      _initialState.push(initState);
+      _currentState.push(initState);
+    }
+
+    _index++;
+
+    //현재 상태값을 저장
+    const state = _currentState[currentIndex];
+
+    // 초기 상태값을 저장하고, 현재 상태값을 관리
+    const setState = (newState) => {
+      if (_currentState[currentIndex] === newState) return;
+      _currentState[currentIndex] = newState;
+      _index = 0;
       callback();
     };
-
     return [state, setState];
   }
 
@@ -26,7 +34,7 @@ export function createHooks(callback) {
 
   const resetContext = () => {
     //현재 상태값을 초기 상태값으로 되돌리는 역할
-    _currentState.length = 0;
+    _index = 0;
   }
 
   return {useState, useMemo, resetContext};
