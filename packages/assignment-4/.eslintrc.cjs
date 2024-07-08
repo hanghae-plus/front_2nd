1f29d99dc453a3611a2db104e9dd335b416c5702
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2024-07-09 00:08:14 +0900

    chore : prettier & eslint

--- packages/assignment-4/.eslintrc.cjs
+++ packages/assignment-4/.eslintrc.cjs
@@ -1,11 +1,22 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2021: true,
+  },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  rules: {},
-}
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
--- packages/assignment-4/src/basic/main.basic.js
+++ packages/assignment-4/src/basic/main.basic.js
@@ -1,9 +1,9 @@
 function main() {
   /** 상품목록*/
   const ITEM_LISTS = [
-    { id: "p1", title: "상품1", price: 10000 },
-    { id: "p2", title: "상품2", price: 20000 },
-    { id: "p3", title: "상품3", price: 30000 },
+    { id: 'p1', title: '상품1', price: 10000 },
+    { id: 'p2', title: '상품2', price: 20000 },
+    { id: 'p3', title: '상품3', price: 30000 },
   ];
 
   /** 할인율 */
@@ -19,47 +19,47 @@ function main() {
   };
 
   /**app root**/
-  const $app = document.getElementById("app");
+  const $app = document.getElementById('app');
 
   /**root내부 회색 배경 */
-  const $background = document.createElement("div");
+  const $background = document.createElement('div');
 
   /**장바구니를 감싸고 있는 card */
-  const $card = document.createElement("div");
+  const $card = document.createElement('div');
 
   /**card내부 title */
-  const $cardTitle = document.createElement("h1");
+  const $cardTitle = document.createElement('h1');
 
   /**장바구니에 담긴 item div */
-  const $cart = document.createElement("div");
+  const $cart = document.createElement('div');
 
   /**가격 총액을 담고 있는 div */
-  const $discountedTotalPrice = document.createElement("div");
+  const $discountedTotalPrice = document.createElement('div');
 
   /** 상품 목록 select*/
-  const $itemSelectbox = document.createElement("select");
+  const $itemSelectbox = document.createElement('select');
 
   /**상품 추가 버튼 */
-  const $addItemButton = document.createElement("button");
+  const $addItemButton = document.createElement('button');
 
   // id나 class를 어떻게 정리해야할지 고민중..(상수이긴 한데..)
-  $cart.id = "cart-items";
-  $discountedTotalPrice.id = "cart-total";
-  $itemSelectbox.id = "product-select";
-  $addItemButton.id = "add-to-cart";
-  $background.className = "bg-gray-100 p-8";
+  $cart.id = 'cart-items';
+  $discountedTotalPrice.id = 'cart-total';
+  $itemSelectbox.id = 'product-select';
+  $addItemButton.id = 'add-to-cart';
+  $background.className = 'bg-gray-100 p-8';
   $card.className =
-    "max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl p-8";
-  $cardTitle.className = "text-2xl font-bold $minusButton-4";
-  $discountedTotalPrice.className = "text-xl font-bold my-4";
-  $itemSelectbox.className = "border rounded p-2 mr-2";
-  $addItemButton.className = "bg-blue-500 text-white px-4 py-2 rounded";
-  $cardTitle.textContent = "장바구니";
-  $addItemButton.textContent = "추가";
+    'max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl p-8';
+  $cardTitle.className = 'text-2xl font-bold $minusButton-4';
+  $discountedTotalPrice.className = 'text-xl font-bold my-4';
+  $itemSelectbox.className = 'border rounded p-2 mr-2';
+  $addItemButton.className = 'bg-blue-500 text-white px-4 py-2 rounded';
+  $cardTitle.textContent = '장바구니';
+  $addItemButton.textContent = '추가';
 
   // 상품목록 select에 option을 넘어주는 반복문
   ITEM_LISTS.forEach((item) => {
-    const $selectOption = document.createElement("option");
+    const $selectOption = document.createElement('option');
     $selectOption.value = item.id;
     $selectOption.textContent = `${item.title}-${item.price}+원`;
     $itemSelectbox.appendChild($selectOption);
@@ -88,7 +88,7 @@ function main() {
       const item = ITEM_LISTS.find((item) => item.id === items[m].id);
 
       const quantity = parseInt(
-        items[m].querySelector("span").textContent.split("x ")[1]
+        items[m].querySelector('span').textContent.split('x ')[1]
       );
 
       /**해당 id 아이템의 총 수량 */
@@ -132,8 +132,8 @@ function main() {
       discountedTotalPrice
     )}원`;
     if (discountRate > 0) {
-      const $discountSpan = document.createElement("span");
-      $discountSpan.className = "text-green-500 ml-2";
+      const $discountSpan = document.createElement('span');
+      $discountSpan.className = 'text-green-500 ml-2';
       $discountSpan.textContent = `(${(discountRate * 100).toFixed(
         1
       )}% 할인 적용)`;
@@ -141,7 +141,7 @@ function main() {
     }
   }
 
-  $addItemButton.addEventListener("click", () => addItemHandler());
+  $addItemButton.addEventListener('click', () => addItemHandler());
 
   /**추가 버튼 이벤트 핸들러 */
   function addItemHandler() {
@@ -157,52 +157,52 @@ function main() {
       //선택한 상품이 이미 장바구니에 있다면??
       if ($targetItem) {
         const [_, currentQuantity] = $targetItem
-          .querySelector("span")
-          .textContent.split("x ");
+          .querySelector('span')
+          .textContent.split('x ');
 
         const quantity = parseInt(currentQuantity) + 1;
         $targetItem.querySelector(
-          "span"
+          'span'
         ).textContent = `${targetItem.title} - ${targetItem.price}원 x ${quantity}`;
       }
       //선택한 상품이 장바구니에 없다면??
       else {
         /**상품의 id를 해당 태그 id로 지정 */
-        const $itemList = document.createElement("div");
+        const $itemList = document.createElement('div');
         /**장바구니 상품 정보 span */
-        const $itemInform = document.createElement("span");
+        const $itemInform = document.createElement('span');
         /**장바구니 아이템에 버튼을 grouping하는 div */
-        const $buttonGroup = document.createElement("div");
+        const $buttonGroup = document.createElement('div');
         /**상품 -버튼 */
-        const $minusButton = document.createElement("button");
+        const $minusButton = document.createElement('button');
         /**상품 +버튼 */
-        const $plusButton = document.createElement("button");
+        const $plusButton = document.createElement('button');
         /**상품 삭제 버튼 */
-        const $removeButton = document.createElement("button");
+        const $removeButton = document.createElement('button');
 
         $itemList.id = targetItem.id;
         $itemList.className =
-          "flex justify-between items-center $minusButton-2";
+          'flex justify-between items-center $minusButton-2';
         $itemInform.textContent =
-          targetItem.title + " - " + targetItem.price + "원 x 1";
+          targetItem.title + ' - ' + targetItem.price + '원 x 1';
         //상품 +버튼 관련
         $minusButton.className =
-          "quantity-change bg-blue-500 text-white px-2 py-1 rounded mr-1";
-        $minusButton.textContent = "-";
+          'quantity-change bg-blue-500 text-white px-2 py-1 rounded mr-1';
+        $minusButton.textContent = '-';
         $minusButton.dataset.productId = targetItem.id;
-        $minusButton.dataset.change = "-1";
+        $minusButton.dataset.change = '-1';
 
         //상품 +버튼 관련
         $plusButton.className =
-          "quantity-change bg-blue-500 text-white px-2 py-1 rounded mr-1";
-        $plusButton.textContent = "+";
+          'quantity-change bg-blue-500 text-white px-2 py-1 rounded mr-1';
+        $plusButton.textContent = '+';
         $plusButton.dataset.productId = targetItem.id;
-        $plusButton.dataset.change = "1";
+        $plusButton.dataset.change = '1';
 
         //상품 삭제 버튼 관련
         $removeButton.className =
-          "remove-item bg-red-500 text-white px-2 py-1 rounded";
-        $removeButton.textContent = "삭제";
+          'remove-item bg-red-500 text-white px-2 py-1 rounded';
+        $removeButton.textContent = '삭제';
         $removeButton.dataset.productId = targetItem.id;
         $buttonGroup.appendChild($minusButton);
         $buttonGroup.appendChild($plusButton);
@@ -217,15 +217,15 @@ function main() {
   }
 
   /**장바구니에 담긴 상품 버튼들에 대한 이벤트(+,-,삭제 버튼) */
-  $cart.addEventListener("click", (event) => cartItemButtonHandler(event));
+  $cart.addEventListener('click', (event) => cartItemButtonHandler(event));
   function cartItemButtonHandler({ target }) {
     /**
      * 버튼의 class에 'quantity-chagne'(+,- 버튼) 혹은
      * 'remove-item'(삭제 버튼)이 포함되어 있다면??
      */
     if (
-      target.classList.contains("quantity-change") ||
-      target.classList.contains("remove-item")
+      target.classList.contains('quantity-change') ||
+      target.classList.contains('remove-item')
     ) {
       const productId = target.dataset.productId;
 
@@ -233,7 +233,7 @@ function main() {
       const item = document.getElementById(productId);
 
       // +,- 버튼일 경우
-      if (target.classList.contains("quantity-change")) {
+      if (target.classList.contains('quantity-change')) {
         /**해당 버튼의 change dataset
          * -버튼의 경우 data property로 -1
          * +버튼의 경우 data property로 +1
@@ -244,8 +244,8 @@ function main() {
 
         // 해당 아이템 구조분해
         const [targetItemInform, currentQuantity] = item
-          .querySelector("span")
-          .textContent.split("x ");
+          .querySelector('span')
+          .textContent.split('x ');
 
         /**해당 아이템의 총 수량 */
         const quantity = Number(currentQuantity) + change;
@@ -253,7 +253,7 @@ function main() {
         // 수량이 1개이상인 경우
         if (quantity > 0) {
           item.querySelector(
-            "span"
+            'span'
           ).textContent = `${targetItemInform}x ${quantity}`;
         }
         // 수량이 없는 경우
@@ -262,7 +262,7 @@ function main() {
         }
       }
       // 그외(삭제 버튼)의 경우
-      else if (target.classList.contains("remove-item")) {
+      else if (target.classList.contains('remove-item')) {
         item.remove();
       }
       updateTotalPrice();
