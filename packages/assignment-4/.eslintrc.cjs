26b16464cb8cb26ef9f94a2355223489cefba417
김자연 <jykim.developer@gmail.com>
2024-07-08 21:43:39 +0900

    chore : Set up eslint, prettier

--- packages/assignment-4/.eslintrc.cjs
+++ packages/assignment-4/.eslintrc.cjs
@@ -3,9 +3,19 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  rules: {},
-}
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+  },
+  plugins: ['react', 'prettier'],
+};
