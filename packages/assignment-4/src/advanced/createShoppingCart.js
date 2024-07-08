80a298fc7a89ad73d411b870ac0098bbfcca0b4a
LucetTin5 <lucettin5@gmail.com>
2024-07-08 17:27:45 +0900

    advanced start

--- packages/assignment-4/src/advanced/createShoppingCart.js
+++ packages/assignment-4/src/advanced/createShoppingCart.js
@@ -1,25 +1,64 @@
+import { products } from './products.js';
+import { DISCOUNT_RATES } from './constants.js';
+
 export const createShoppingCart = () => {
+  // cart에 들어있는 items
   const items = {};
 
-  const addItem = () => {};
+  const addItem = (productId) => {
+    if (!items[productId]) {
+      items[productId] = { quantity: 1 };
+    } else {
+      items[productId].quantity += 1;
+    }
+  };
 
-  const removeItem = () => {};
+  const removeItem = (productId) => {
+    delete items[productId];
+  };
 
-  const updateQuantity = () => {};
+  // +와 - 버튼에 대해서만 기능하도록 하며, 신규 추가에 대해서는 위의 addItem이 사용되도록 한다.
+  const updateQuantity = ({ updateType, productId }) => {
+    if (updateType === 'increase') {
+      items[productId].quantity += 1;
+      return;
+    }
+    if (updateType === 'decrease') {
+      if (items[productId].quantity === 1) return removeItem(productId);
+      items[productId].quantity -= 1;
+      return;
+    }
+  };
 
-  const getItems = () => [];
+  const getItems = () => {
+    const result = [];
+    const productIds = Object.keys(items);
+    productIds.forEach((productId) => {
+      result.push({ productId, quantity: items[productId].quantity });
+    });
+    return result;
+  };
 
   const calculateDiscount = () => {
     return 0;
   };
 
+  const getTotalQuantity = () => {
+    const productIds = Object.keys(items);
+    return productIds.reduce((sum, productId) => sum + items[productId].quantity, 0);
+  };
 
-  const getTotalQuantity = () => 0;
-
-  const getTotal = () => ({
-    total: 0,
-    discountRate: 0
-  });
+  const getTotal = () => {
+    const totalPriceBeforeDiscount = Object.keys(items).reduce((sum, productId) => {
+      const { price } = products.find((product) => product.productId === productId);
+      const { quantity } = items[productId];
+      return sum + price * quantity;
+    }, 0);
+    return {
+      total: 0,
+      discountRate: 0,
+    };
+  };
 
   return { addItem, removeItem, updateQuantity, getItems, getTotal };
 };
---
+++ packages/assignment-4/src/advanced/products.js
@@ -0,0 +1,17 @@
+export const products = [
+  {
+    productId: 'p1',
+    productName: '상품1',
+    price: 10000,
+  },
+  {
+    productId: 'p2',
+    productName: '상품2',
+    price: 20000,
+  },
+  {
+    productId: 'p3',
+    productName: '상품3',
+    price: 30000,
+  },
+];
