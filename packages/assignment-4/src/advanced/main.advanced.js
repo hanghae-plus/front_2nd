6ad9f3f59801889a4717499eda135feda0ac7e23
LucetTin5 <lucettin5@gmail.com>
2024-07-09 17:59:38 +0900

    re-create advanced base

--- packages/assignment-4/src/advanced/main.advanced.js
+++ packages/assignment-4/src/advanced/main.advanced.js
@@ -1,13 +1,3 @@
-import { createCartView } from './createCartView.js';
-
-function main() {
-  const appRoute = document.querySelector('#app');
-  const { renderCartItems, renderCartPrice } = createCartView(appRoute);
-
-  window.addEventListener('cartChange', () => {
-    renderCartItems();
-    renderCartPrice();
-  });
-}
+function main() {}
 
 main();
