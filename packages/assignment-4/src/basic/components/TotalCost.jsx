591414a577e8ef3df0cea6c7fb16bad0dca131f9
mimi <62527898+HM970506@users.noreply.github.com>
2024-07-10 21:20:28 +0900

    refactor: SOLID

--- packages/assignment-4/src/basic/components/TotalCost.jsx
+++ packages/assignment-4/src/basic/components/TotalCost.jsx
@@ -5,11 +5,10 @@ export default function TotalCost({ cart }) {
   const [discount, setDiscount] = useState(0);
 
   useEffect(() => {
-    const { nowTotalCost, dcTotalCost, totalCnt } = calculateTotalCost(cart);
+    const dcTotalCost = calculateTotalCost(cart);
     const { conclusedTotalCost, totalDc } = calculateDiscount(
-      nowTotalCost,
-      dcTotalCost,
-      totalCnt
+      cart,
+      dcTotalCost
     );
 
     setTotalCost(conclusedTotalCost);
@@ -17,32 +16,24 @@ export default function TotalCost({ cart }) {
   }, [cart]);
 
   const calculateTotalCost = (nowCart) => {
-    return nowCart.reduce(
-      (accumulator, current) => {
-        let disc = 0;
+    return nowCart.reduce((accumulator, current) => {
+      let disc = 0;
 
-        if (current.cnt >= 10) {
-          if (current.id === "p1") disc = 0.1;
-          else if (current.id === "p2") disc = 0.15;
-          else if (current.id === "p3") disc = 0.2;
-        }
-        const optionTotalCost = current.cost * current.cnt;
-
-        return {
-          nowTotalCost: accumulator.nowTotalCost + optionTotalCost,
-          dcTotalCost: accumulator.dcTotalCost + optionTotalCost * (1 - disc),
-          totalCnt: accumulator.totalCnt + current.cnt,
-        };
-      },
-      {
-        nowTotalCost: 0,
-        dcTotalCost: 0,
-        totalCnt: 0,
+      if (current.cnt >= 10) {
+        if (current.id === "p1") disc = 0.1;
+        else if (current.id === "p2") disc = 0.15;
+        else if (current.id === "p3") disc = 0.2;
       }
-    );
+      const optionTotalCost = current.cost * current.cnt;
+
+      return accumulator + optionTotalCost * (1 - disc);
+    }, 0);
   };
 
-  const calculateDiscount = (nowTotalCost, dcTotalCost, totalCnt) => {
+  const calculateDiscount = (nowCart, dcTotalCost) => {
+    const nowTotalCost = nowCart.reduce((ac, cu) => ac + cu.cost * cu.cnt, 0);
+    const totalCnt = nowCart.reduce((ac, cu) => ac + cu.cnt, 0);
+
     let dc = (nowTotalCost - dcTotalCost) / nowTotalCost;
     let finalDcTotalCost = dcTotalCost;
 
@@ -67,7 +58,7 @@ export default function TotalCost({ cart }) {
         <div id="cart-total" className="text-xl font-bold my-4">
           총액: {totalCost}원
           {discount > 0 && (
-            <span class="text-green-500 ml-2">({discount}% 할인 적용)</span>
+            <span className="text-green-500 ml-2">({discount}% 할인 적용)</span>
           )}
         </div>
       )}
