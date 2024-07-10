98ff305f16ac351ee8b464f5caa7402667ec7f0a
2pandi <coder.2pandi@gmail.com>
2024-07-10 23:24:54 +0900

    eslint, prettier 추가

--- packages/assignment-4/.eslintrc.js
+++ packages/assignment-4/.eslintrc.js
@@ -2,14 +2,13 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
   },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:prettier/recommended',
+    'prettier',
   ],
-  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -19,15 +18,6 @@ module.exports = {
   },
   plugins: ['react', 'prettier'],
   rules: {
-    'no-undef': 'error',
-    'no-unused-vars': 'error',
     'prettier/prettier': 'error',
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
   },
 };
