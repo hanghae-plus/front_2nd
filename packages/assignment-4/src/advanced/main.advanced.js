c9449244818b5b3ab5116ed8de3ce66b317d5a6e
LucetTin5 <Lucettin5@gmail.com>
2024-07-10 20:01:45 +0900

    주석 추가

--- packages/assignment-4/src/advanced/main.advanced.js
+++ packages/assignment-4/src/advanced/main.advanced.js
@@ -1,5 +1,6 @@
 import { createCartView } from './createCartView.js';
 
+// main.js에 있던 products와 같은 형태를 우선 사용
 const products = [
   { productId: 'p1', productName: '상품1', price: 10000, discount: [[10, 0.1]] },
   { productId: 'p2', productName: '상품2', price: 20000, discount: [[10, 0.15]] },
