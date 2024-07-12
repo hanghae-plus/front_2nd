f4da97115f3b37570b1d403ad43a45fc2bcab566
haeji1124 <46364329+haeji1124@users.noreply.github.com>
2024-07-12 09:30:10 +0900

    advanced

--- packages/assignment-4/src/advanced/main.advanced.js
+++ packages/assignment-4/src/advanced/main.advanced.js
@@ -1,5 +1,15 @@
-function main() {
+import createShoppingCart from './createShoppingCart.js';
+import createCartView from './createCartView.js';
+import templates from './templates.js';
 
+function main() {
+    const cart = createShoppingCart();
+    const cartView = createCartView(cart, templates);
+  
+    cartView.render();
+    
+    const cartItemsContainer = document.getElementById('cart-items');
+    cartItemsContainer.addEventListener('click', cartView.handleCartItemAction);
 }
 
 main();
