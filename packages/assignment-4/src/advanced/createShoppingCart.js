d2c15ecbb6c7c3b544ceee63aaba24a1479811bf
metacode22 <alohajune22@gmail.com>
2024-07-13 00:17:15 +0900

    advanced 구현

--- packages/assignment-4/src/advanced/createShoppingCart.js
+++ packages/assignment-4/src/advanced/createShoppingCart.js
@@ -1,25 +1,81 @@
+const TOTAL_DISCOUNT_THRESHOLD = 30;
+const TOTAL_DISCOUNT_RATE = 0.25;
+
 export const createShoppingCart = () => {
-  const items = {};
+  let items = [];
 
-  const addItem = () => {};
-
-  const removeItem = () => {};
-
-  const updateQuantity = () => {};
-
-  const getItems = () => [];
-
-  const calculateDiscount = () => {
-    return 0;
+  const findItem = (productId) => {
+    return items.find(({ product }) => product.id === productId);
   };
 
+  const addItem = (product, quantity) => {
+    const existingItem = findItem(product.id);
 
-  const getTotalQuantity = () => 0;
+    if (existingItem) {
+      existingItem.quantity += quantity || 1;
+
+      return;
+    }
+
+    items.push({ product, quantity: quantity || 1 });
+  };
+
+  const removeItem = (productId) => {
+    items = items.filter(({ product }) => product.id !== productId);
+  };
+
+  const updateQuantity = (productId, quantityChange) => {
+    const item = findItem(productId);
+
+    if (!item) return;
+
+    item.quantity = quantityChange;
+
+    if (item.quantity <= 0) removeItem(productId);
+  };
+
+  const getItems = () => {
+    return items;
+  };
+
+  const getTotalQuantity = () => {
+    return items.reduce((acc, { quantity }) => acc + quantity, 0);
+  };
+
+  const calculateTotalBeforeDiscount = () => {
+    return items.reduce(
+      (acc, { product, quantity }) => acc + product.price * quantity,
+      0
+    );
+  };
+
+  const calculateDiscount = () => {
+    const totalQuantity = getTotalQuantity();
+
+    if (totalQuantity >= TOTAL_DISCOUNT_THRESHOLD) {
+      const totalPrice = items.reduce(
+        (acc, { product, quantity }) => acc + product.price * quantity,
+        0
+      );
+
+      return totalPrice * TOTAL_DISCOUNT_RATE;
+    }
+
+    const totalDiscount = items.reduce((acc, { product, quantity }) => {
+      if (quantity >= product.discount?.[0]?.[0]) {
+        return acc + product.price * quantity * product.discount[0][1];
+      }
+
+      return acc;
+    }, 0);
+
+    return totalDiscount;
+  };
 
   const getTotal = () => ({
-    total: 0,
-    discountRate: 0
+    total: calculateTotalBeforeDiscount() - calculateDiscount(),
+    discountRate: calculateDiscount() / calculateTotalBeforeDiscount(),
   });
 
-  return { addItem, removeItem, updateQuantity, getItems, getTotal };
+  return { findItem, addItem, removeItem, updateQuantity, getItems, getTotal };
 };
