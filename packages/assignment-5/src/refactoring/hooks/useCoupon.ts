c9b14fbb240089ccf4cae1101f3da95323f504f0
mosozeep <108648919+mosohyun@users.noreply.github.com>
2024-07-15 22:47:45 +0900

    feat: useProduct, useCoupon 기능 구현

--- packages/assignment-5/src/refactoring/hooks/useCoupon.ts
+++ packages/assignment-5/src/refactoring/hooks/useCoupon.ts
@@ -1,6 +1,10 @@
-import { Coupon } from "../../types.ts";
-import { useState } from "react";
+import { Coupon } from '../../types.ts';
+import { useState } from 'react';
 
 export const useCoupons = (initialCoupons: Coupon[]) => {
-  return { coupons: [], addCoupon: () => undefined };
+  const [coupons, setCoupons] = useState(initialCoupons);
+  return {
+    coupons,
+    addCoupon: (newCoupon) => setCoupons([...coupons, newCoupon]),
+  };
 };
--- packages/assignment-5/src/refactoring/hooks/useProduct.ts
+++ packages/assignment-5/src/refactoring/hooks/useProduct.ts
@@ -2,5 +2,19 @@ import { useState } from 'react';
 import { Product } from '../../types.ts';
 
 export const useProducts = (initialProducts: Product[]) => {
-  return { products: [], updateProduct: () => undefined, addProduct: () => undefined };
+  const [products, setProducts] = useState(initialProducts);
+
+  return {
+    products,
+    updateProduct: (newProduct) => {
+      const targetIndex = products.findIndex(
+        (product) => product.id === newProduct.id
+      );
+      const newProducts = [...products];
+      newProducts.splice(targetIndex, 2, newProduct);
+
+      setProducts(newProducts);
+    },
+    addProduct: (newProduct) => setProducts([...products, { ...newProduct }]),
+  };
 };
