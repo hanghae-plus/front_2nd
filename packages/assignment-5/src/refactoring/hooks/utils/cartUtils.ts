d1aa4beab049d9e8ad3c7d1e09df4188ea72aaf8
JIHAN <74421125+bbbjihan@users.noreply.github.com>
2024-07-15 18:15:20 +0900

    5주차 > basic > hooks 및 utils 작성

--- packages/assignment-5/src/refactoring/hooks/utils/cartUtils.ts
+++ packages/assignment-5/src/refactoring/hooks/utils/cartUtils.ts
@@ -1,21 +1,80 @@
 import { CartItem, Coupon } from "../../../types";
 
 export const calculateItemTotal = (item: CartItem) => {
-  return 0;
+  const discountRate = getMaxApplicableDiscount(item);
+  const totalPrice = item.product.price * item.quantity;
+
+  return totalPrice * (1 - discountRate);
 };
 
 export const getMaxApplicableDiscount = (item: CartItem) => {
-  return 0;
+  const { discounts } = item.product;
+
+  if (discounts.length === 0) {
+    return 0;
+  }
+
+  for (let index = 0; index < discounts.length; index++) {
+    if (discounts[index].quantity > item.quantity) {
+      return index === 0 ? 0 : discounts[index - 1].rate;
+    }
+  }
+
+  return discounts[discounts.length - 1].rate;
 };
 
-export const calculateCartTotal = (cart: CartItem[], selectedCoupon: Coupon | null) => {
+export const calculateCartTotal = (
+  cart: CartItem[],
+  selectedCoupon: Coupon | null
+) => {
+  const totalBeforeDiscount = cart.reduce(
+    (acc, item) => acc + item.product.price * item.quantity,
+    0
+  );
+
+  const totalBeforeCoupon = cart.reduce(
+    (acc, item) => acc + calculateItemTotal(item),
+    0
+  );
+
+  const totalAfterDiscount = selectedCoupon
+    ? selectedCoupon.discountType === "amount"
+      ? totalBeforeCoupon - selectedCoupon.discountValue
+      : (totalBeforeCoupon * (100 - selectedCoupon.discountValue)) / 100
+    : totalBeforeCoupon;
+
+  const totalDiscount = totalBeforeDiscount - totalAfterDiscount;
+
   return {
-    totalBeforeDiscount: 0,
-    totalAfterDiscount: 0,
-    totalDiscount: 0,
+    totalBeforeDiscount,
+    totalAfterDiscount,
+    totalDiscount,
   };
 };
 
-export const updateCartItemQuantity = (cart: CartItem[], productId: string, newQuantity: number): CartItem[] => {
-  return []
+export const updateCartItemQuantity = (
+  cart: CartItem[],
+  productId: string,
+  newQuantity: number
+): CartItem[] => {
+  const targetProductIndex = cart.findIndex(
+    (item) => item.product.id === productId
+  );
+
+  if (targetProductIndex < 0) {
+    return cart;
+  }
+
+  const newCart = [...cart];
+
+  if (newQuantity === 0) {
+    newCart.splice(targetProductIndex, 1);
+  }
+
+  newCart[targetProductIndex].quantity =
+    newQuantity > newCart[targetProductIndex].product.stock
+      ? newCart[targetProductIndex].product.stock
+      : newQuantity;
+
+  return newCart;
 };
