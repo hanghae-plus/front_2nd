8fcdcf7b6dbcc9e0314b9586673c4a3f0d3f5ed9
mosozeep <108648919+mosohyun@users.noreply.github.com>
2024-07-16 21:29:15 +0900

    feat: 커스텀 훅 추가
    - useCart 기능 추가

--- packages/assignment-5/src/refactoring/hooks/useCart.ts
+++ packages/assignment-5/src/refactoring/hooks/useCart.ts
@@ -7,19 +7,65 @@ export const useCart = () => {
   const [cart, setCart] = useState<CartItem[]>([]);
   const [selectedCoupon, setSelectedCoupon] = useState<Coupon | null>(null);
 
-  const addToCart = (product: Product) => {};
+  const addToCart = (product: Product) => {
+    setCart([...cart, { product, quantity: 1 }]);
+  };
 
-  const removeFromCart = (productId: string) => {};
+  const removeFromCart = (productId: string) => {
+    setCart(cart.filter((item) => item.product.id !== productId));
+  };
 
-  const updateQuantity = (productId: string, newQuantity: number) => {};
+  const updateQuantity = (productId: string, newQuantity: number) => {
+    setCart((prevCart) => {
+      return prevCart.map((item) =>
+        item.product.id === productId
+          ? { ...item, quantity: newQuantity }
+          : item
+      );
+    });
+  };
 
-  const applyCoupon = (coupon: Coupon) => {};
+  const applyCoupon = (coupon: Coupon) => {
+    setSelectedCoupon(coupon);
+  };
 
-  const calculateTotal = () => ({
-    totalBeforeDiscount: 0,
-    totalAfterDiscount: 0,
-    totalDiscount: 0,
-  })
+  const getTotalPrice = (rate = 1) => {
+    return cart.reduce(
+      ([before, after], { product, quantity }) => [
+        before + product.price * quantity,
+        after + product.price * quantity * (1 - rate),
+      ],
+      [0, 0]
+    );
+  };
+
+  const calculateTotal = () => {
+    let rate = 1;
+
+    if (
+      selectedCoupon !== null &&
+      selectedCoupon.discountType === 'percentage'
+    ) {
+      rate = selectedCoupon.discountValue / 100;
+    }
+
+    let [totalBeforeDiscount, totalAfterDiscount] = getTotalPrice(rate);
+
+    if (selectedCoupon?.discountType === 'amount') {
+      totalAfterDiscount = Math.max(
+        0,
+        totalAfterDiscount - selectedCoupon.discountValue
+      );
+    }
+
+    const totalDiscount = totalBeforeDiscount - totalAfterDiscount;
+
+    return {
+      totalBeforeDiscount: Math.round(totalBeforeDiscount),
+      totalAfterDiscount: Math.round(totalAfterDiscount),
+      totalDiscount: Math.round(totalDiscount),
+    };
+  };
 
   return {
     cart,
