be222f2108d307647057d93aa422e0e92741f4ce
seojunhwan <seojunhwan@kakao.com>
2024-07-16 23:29:21 +0900

    feat: cart utils 구현

--- packages/assignment-5/src/refactoring/hooks/utils/cartUtils.ts
+++ packages/assignment-5/src/refactoring/hooks/utils/cartUtils.ts
@@ -1,21 +1,57 @@
-import { CartItem, Coupon } from "../../../types";
+import { CartItem, Coupon } from '../../../types';
 
 export const calculateItemTotal = (item: CartItem) => {
-  return 0;
+  const { quantity } = item;
+  const { price } = item.product;
+
+  const discount = getMaxApplicableDiscount(item);
+
+  return price * quantity * (1 - discount);
 };
 
 export const getMaxApplicableDiscount = (item: CartItem) => {
-  return 0;
+  const {
+    product: { discounts },
+    quantity,
+  } = item;
+
+  return discounts.reduce((maxDiscount, discount) => {
+    return quantity >= discount.quantity && discount.rate > maxDiscount ? discount.rate : maxDiscount;
+  }, 0);
 };
 
 export const calculateCartTotal = (cart: CartItem[], selectedCoupon: Coupon | null) => {
+  const totalBeforeDiscount = cart.reduce((total, item) => total + item.product.price * item.quantity, 0);
+
+  let totalAfterDiscount = cart.reduce((total, item) => total + calculateItemTotal(item), 0);
+
+  // 쿠폰 적용
+  if (selectedCoupon) {
+    if (selectedCoupon.discountType === 'amount') {
+      totalAfterDiscount = Math.max(0, totalAfterDiscount - selectedCoupon.discountValue);
+    } else if (selectedCoupon.discountType === 'percentage') {
+      totalAfterDiscount *= 1 - selectedCoupon.discountValue / 100;
+    }
+  }
+
+  const totalDiscount = totalBeforeDiscount - totalAfterDiscount;
+
   return {
-    totalBeforeDiscount: 0,
-    totalAfterDiscount: 0,
-    totalDiscount: 0,
+    totalBeforeDiscount: Math.round(totalBeforeDiscount),
+    totalAfterDiscount: Math.round(totalAfterDiscount),
+    totalDiscount: Math.round(totalDiscount),
   };
 };
 
 export const updateCartItemQuantity = (cart: CartItem[], productId: string, newQuantity: number): CartItem[] => {
-  return []
+  return cart
+    .map((item) => {
+      if (item.product.id === productId) {
+        const maxQuantity = item.product.stock;
+        const updatedQuantity = Math.max(0, Math.min(newQuantity, maxQuantity));
+        return updatedQuantity > 0 ? { ...item, quantity: updatedQuantity } : null;
+      }
+      return item;
+    })
+    .filter((item): item is CartItem => item !== null);
 };
