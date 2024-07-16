46c9cda428082390a701ff320ddd44a6a9251072
2pandi <coder.2pandi@gmail.com>
2024-07-16 21:37:09 +0900

    장바구니 페이지 테스트 통과

--- packages/assignment-5/src/refactoring/hooks/useCart.ts
+++ packages/assignment-5/src/refactoring/hooks/useCart.ts
@@ -1,39 +1,54 @@
 // useCart.ts
 import { useState } from 'react';
 import { CartItem, Coupon, Product } from '../../types';
-import { calculateCartTotal, getCartItem, updateCartItemQuantity } from './utils/cartUtils';
-import { addQuantity } from '../constants';
+import {
+  calculateCartTotal,
+  getCartItem,
+  updateCartItemQuantity,
+} from './utils/cartUtils';
+import { MODIFY_QUANTITY } from '../constants';
 
 export const useCart = () => {
   const [cart, setCart] = useState<CartItem[]>([]);
   const [selectedCoupon, setSelectedCoupon] = useState<Coupon | null>(null);
-  
+
   const addToCart = (product: Product) => {
-    const newCartItem = getCartItem(cart, product)
-    newCartItem.quantity += addQuantity
+    let hasCartItem = false;
+    const newCartItem = getCartItem(cart, product);
+    newCartItem.quantity += MODIFY_QUANTITY;
     const newCart = cart.map((cartItem) => {
-      if(cartItem.product.id === product.id) return newCartItem
-      return cartItem
-    })
-    if(!newCart.length) newCart.push(newCartItem)
+      if (cartItem.product.id === product.id) {
+        hasCartItem = true;
+        return newCartItem;
+      }
+      return cartItem;
+    });
+    if (!hasCartItem) newCart.push(newCartItem);
     setCart(newCart);
   };
 
-  const removeFromCart = (productId: string) => {};
+  const removeFromCart = (productId: string) => {
+    const newCart = cart.filter((item) => item.product.id !== productId);
+    setCart(newCart);
+  };
 
-  const updateQuantity = (productId: string, newQuantity: number) => {};
+  const updateQuantity = (productId: string, newQuantity: number) => {
+    const cartItem = getCartItem(
+      cart,
+      cart.find((item) => item.product.id === productId)?.product as Product,
+    );
+    if (newQuantity > cartItem.product.stock) return;
+    if (newQuantity <= 0) return removeFromCart(productId);
+    setCart(updateCartItemQuantity(cart, productId, newQuantity));
+  };
 
-  const applyCoupon = (coupon: Coupon) => {};
+  const applyCoupon = (coupon: Coupon) => {
+    setSelectedCoupon(coupon);
+  };
 
   const calculateTotal = () => {
-    
-    
-    return {
-    totalBeforeDiscount: 0,
-    totalAfterDiscount: 0,
-    totalDiscount: 0,
+    return calculateCartTotal(cart, selectedCoupon);
   };
-}
 
   return {
     cart,
--- packages/assignment-5/src/refactoring/hooks/utils/cartUtils.ts
+++ packages/assignment-5/src/refactoring/hooks/utils/cartUtils.ts
@@ -1,28 +1,65 @@
-import { CartItem, Coupon, Product } from "../../../types";
-import { initQuantity } from "../../constants";
+import { CartItem, Coupon, Product } from '../../../types';
+import { AMOUNT5000, INIT_QUANTITY, PERCENT10 } from '../../constants';
 
 export const getCartItem = (cart: CartItem[], product: Product) => {
-  const cartItem = cart.find((item) => item.product.id === product.id)
-  return cartItem ? cartItem : {product, quantity: initQuantity}
-  
-}
+  const cartItem = cart.find((item) => item.product.id === product.id);
+  return cartItem ? cartItem : { product, quantity: INIT_QUANTITY };
+};
 
 export const calculateItemTotal = (item: CartItem) => {
-  return 0;
+  return item.product.price * item.quantity;
 };
 
 export const getMaxApplicableDiscount = (item: CartItem) => {
-  return 0;
+  return item.product.discounts.reduce(
+    (pre, cur) => (item.quantity >= cur.quantity ? cur.rate : pre),
+    0,
+  );
 };
 
-export const calculateCartTotal = (cart: CartItem[], selectedCoupon: Coupon | null) => {
+export const calculateCartTotal = (
+  cart: CartItem[],
+  selectedCoupon: Coupon | null,
+) => {
+  const totalBeforeDiscount = cart.reduce(
+    (pre, cur) => pre + calculateItemTotal(cur),
+    0,
+  );
+  let totalDiscount = cart
+    .map(
+      (item) =>
+        getMaxApplicableDiscount(item) * item.product.price * item.quantity,
+    )
+    .reduce((pre, cur) => pre + cur, 0);
+
+  if (selectedCoupon) {
+    if (selectedCoupon.code === AMOUNT5000)
+      totalDiscount += selectedCoupon.discountValue;
+    if (selectedCoupon.code === PERCENT10) {
+      totalDiscount +=
+        ((totalBeforeDiscount - totalDiscount) * selectedCoupon.discountValue) /
+        100;
+    }
+  }
+
+  const totalAfterDiscount = totalBeforeDiscount - totalDiscount;
+
   return {
-    totalBeforeDiscount: 0,
-    totalAfterDiscount: 0,
-    totalDiscount: 0,
+    totalBeforeDiscount,
+    totalAfterDiscount,
+    totalDiscount,
   };
 };
 
-export const updateCartItemQuantity = (cart: CartItem[], productId: string, newQuantity: number): CartItem[] => {
-  return []
+export const updateCartItemQuantity = (
+  cart: CartItem[],
+  productId: string,
+  newQuantity: number,
+): CartItem[] => {
+  return cart.map((item) => {
+    if (item.product.id === productId) {
+      item.quantity = newQuantity;
+    }
+    return item;
+  });
 };
