d35b1348a9f9ed978c93fd5cbeb7542258b54141
metacode22 <alohajune22@gmail.com>
2024-07-18 00:44:15 +0900

    feat: useCart, useProduct, useCoupon, cartUtils 구현

--- packages/assignment-5/src/refactoring/hooks/useProduct.ts
+++ packages/assignment-5/src/refactoring/hooks/useProduct.ts
@@ -1,6 +1,14 @@
 import { useState } from 'react';
+
 import { Product } from '../../types.ts';
 
 export const useProducts = (initialProducts: Product[]) => {
-  return { products: [], updateProduct: () => undefined, addProduct: () => undefined };
+  const [products, setProducts] = useState(initialProducts);
+
+  const updateProduct = (newProduct: Product) =>
+    setProducts((prevProducts) => prevProducts.map((product) => (product.id === newProduct.id ? newProduct : product)));
+
+  const addProduct = (newProduct: Product) => setProducts((prevProducts) => [...prevProducts, newProduct]);
+
+  return { products, updateProduct, addProduct };
 };
