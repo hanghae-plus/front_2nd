2207fd8a19cb4129c22fa79760f6fd52691a4a3d
metacode22 <alohajune22@gmail.com>
2024-07-18 00:44:15 +0900

    feat: useCart, useProduct, useCoupon, cartUtils 구현

--- packages/assignment-5/src/refactoring/hooks/useCart.ts
+++ packages/assignment-5/src/refactoring/hooks/useCart.ts
@@ -1,5 +1,6 @@
 // useCart.ts
 import { useState } from 'react';
+
 import { CartItem, Coupon, Product } from '../../types';
 import { calculateCartTotal, updateCartItemQuantity } from './utils/cartUtils';
 
@@ -7,19 +8,36 @@ export const useCart = () => {
   const [cart, setCart] = useState<CartItem[]>([]);
   const [selectedCoupon, setSelectedCoupon] = useState<Coupon | null>(null);
 
-  const addToCart = (product: Product) => {};
+  const addToCart = (product: Product) => {
+    const cartItem = cart.find((item) => item.product.id === product.id);
+    const remainingStock = product.stock - (cartItem?.quantity ?? 0);
+    if (remainingStock <= 0) return;
 
-  const removeFromCart = (productId: string) => {};
+    setCart((previousCart) => {
+      const existingCartItem = previousCart.find((item) => item.product.id === product.id);
+      if (existingCartItem) {
+        return updateCartItemQuantity(previousCart, product.id, existingCartItem.quantity + 1);
+      }
 
-  const updateQuantity = (productId: string, newQuantity: number) => {};
+      return [...previousCart, { product, quantity: 1 }];
+    });
+  };
 
-  const applyCoupon = (coupon: Coupon) => {};
+  const removeFromCart = (productId: string) => {
+    setCart((previousCart) => previousCart.filter((item) => item.product.id !== productId));
+  };
 
-  const calculateTotal = () => ({
-    totalBeforeDiscount: 0,
-    totalAfterDiscount: 0,
-    totalDiscount: 0,
-  })
+  const updateQuantity = (productId: string, newQuantity: number) => {
+    setCart((previousCart) => updateCartItemQuantity(previousCart, productId, newQuantity));
+  };
+
+  const applyCoupon = (coupon: Coupon) => {
+    setSelectedCoupon(coupon);
+  };
+
+  const calculateTotal = () => {
+    return calculateCartTotal(cart, selectedCoupon);
+  };
 
   return {
     cart,
@@ -28,6 +46,6 @@ export const useCart = () => {
     updateQuantity,
     applyCoupon,
     calculateTotal,
-    selectedCoupon,
+    selectedCoupon
   };
 };
