dfd02f1c94cf24b23dbef9fa3f363f9e40d66c0b
mosozeep <108648919+mosohyun@users.noreply.github.com>
2024-07-17 20:17:40 +0900

    fix: useCart 수정
    - cartUtil 기능 사용

--- packages/assignment-5/src/refactoring/hooks/useCart.ts
+++ packages/assignment-5/src/refactoring/hooks/useCart.ts
@@ -16,55 +16,17 @@ export const useCart = () => {
   };
 
   const updateQuantity = (productId: string, newQuantity: number) => {
-    setCart((prevCart) => {
-      return prevCart.map((item) =>
-        item.product.id === productId
-          ? { ...item, quantity: newQuantity }
-          : item
-      );
-    });
+    setCart((prevCart) =>
+      updateCartItemQuantity(prevCart, productId, newQuantity)
+    );
   };
 
   const applyCoupon = (coupon: Coupon) => {
     setSelectedCoupon(coupon);
   };
 
-  const getTotalPrice = (rate = 1) => {
-    return cart.reduce(
-      ([before, after], { product, quantity }) => [
-        before + product.price * quantity,
-        after + product.price * quantity * (1 - rate),
-      ],
-      [0, 0]
-    );
-  };
-
   const calculateTotal = () => {
-    let rate = 1;
-
-    if (
-      selectedCoupon !== null &&
-      selectedCoupon.discountType === 'percentage'
-    ) {
-      rate = selectedCoupon.discountValue / 100;
-    }
-
-    let [totalBeforeDiscount, totalAfterDiscount] = getTotalPrice(rate);
-
-    if (selectedCoupon?.discountType === 'amount') {
-      totalAfterDiscount = Math.max(
-        0,
-        totalAfterDiscount - selectedCoupon.discountValue
-      );
-    }
-
-    const totalDiscount = totalBeforeDiscount - totalAfterDiscount;
-
-    return {
-      totalBeforeDiscount: Math.round(totalBeforeDiscount),
-      totalAfterDiscount: Math.round(totalAfterDiscount),
-      totalDiscount: Math.round(totalDiscount),
-    };
+    return calculateCartTotal(cart, selectedCoupon);
   };
 
   return {
