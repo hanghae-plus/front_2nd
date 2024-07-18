80c15f2533843730c641c6f055932496972b4415
Rachel <borapark13@gmail.com>
2024-07-18 22:32:25 +0900

    style: auto-import lint settings

--- packages/assignment-5/src/advanced/__tests__/advanced.test.tsx
+++ packages/assignment-5/src/advanced/__tests__/advanced.test.tsx
@@ -1,8 +1,9 @@
+import { act, fireEvent, render, screen, within } from '@testing-library/react';
 import { useState } from 'react';
 import { describe, expect, test } from 'vitest';
-import { act, fireEvent, render, screen, within } from '@testing-library/react';
-import { CartPage } from '../../refactoring/components/CartPage';
+
 import { AdminPage } from '../../refactoring/components/AdminPage';
+import { CartPage } from '../../refactoring/components/CartPage';
 import { Coupon, Product } from '../../types';
 
 const mockProducts: Product[] = [
