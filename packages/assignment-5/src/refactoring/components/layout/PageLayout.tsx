db4e065cb299b2d2c787de86a8e10e9035a4d00e
LucetTin5 <Lucettin5@gmail.com>
2024-07-18 23:17:48 +0900

    fix: 오타 수정

--- packages/assignment-5/src/refactoring/components/layout/PageLayout.tsx
+++ packages/assignment-5/src/refactoring/components/layout/PageLayout.tsx
@@ -7,7 +7,7 @@ interface PageLayout {
 
 export const PageLayout = ({ title, children }: PageLayout) => {
   return (
-    <div className="containter mx-auto p-4">
+    <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-6">{title}</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">{children}</div>
     </div>
