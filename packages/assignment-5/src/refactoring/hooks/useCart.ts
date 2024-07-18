998c1b60b70b5effd011e12f10bd8efeafa068fd
김자연 <jykim.developer@gmail.com>
2024-07-19 00:37:49 +0900

    feat : 5주차 basic

--- packages/assignment-5/src/refactoring/hooks/useCart.ts
+++ packages/assignment-5/src/refactoring/hooks/useCart.ts
@@ -1,33 +1,57 @@
 // useCart.ts
-import { useState } from 'react';
-import { CartItem, Coupon, Product } from '../../types';
-import { calculateCartTotal, updateCartItemQuantity } from './utils/cartUtils';
+import { useState } from "react";
+import { CartItem, Coupon, Product } from "../../types";
+import { calculateCartTotal, updateCartItemQuantity } from "./utils/cartUtils";
 
 export const useCart = () => {
   const [cart, setCart] = useState<CartItem[]>([]);
   const [selectedCoupon, setSelectedCoupon] = useState<Coupon | null>(null);
 
-  const addToCart = (product: Product) => {};
+  const addToCart = (product: Product) => {
+    // const remainingStock = getRemainingStock(product);
+    // if (remainingStock <= 0) return;
 
-  const removeFromCart = (productId: string) => {};
+    setCart((prevCart) => {
+      const existingItem = prevCart.find((item) => item.product.id === product.id);
+      if (existingItem) {
+        // 기존에 동일한 상품이 카트에 있는 경우, 수량을 증가시키며, 상품의 최대 수량을 넘지 않도록
+        return prevCart.map((item) =>
+          item.product.id === product.id ? { ...item, quantity: Math.min(item.quantity + 1, product.stock) } : item
+        );
+      }
+      // 동일한 상품이 카트에 없는 경우, 새로운 상품을 수량 1로 추가
+      return [...prevCart, { product, quantity: 1 }];
+    });
+  };
 
-  const updateQuantity = (productId: string, newQuantity: number) => {};
+  const removeFromCart = (productId: string) => {
+    setCart((prevCart) => prevCart.filter((item) => item.product.id !== productId));
+  };
 
-  const applyCoupon = (coupon: Coupon) => {};
+  const updateQuantity = (productId: string, newQuantity: number) => {
+    setCart((prevCart) => updateCartItemQuantity(prevCart, productId, newQuantity));
+  };
 
-  const calculateTotal = () => ({
-    totalBeforeDiscount: 0,
-    totalAfterDiscount: 0,
-    totalDiscount: 0,
-  })
+  const applyCoupon = (coupon: Coupon) => {
+    setSelectedCoupon(coupon);
+  };
+
+  const calculateTotal = () => {
+    const { totalBeforeDiscount, totalAfterDiscount, totalDiscount } = calculateCartTotal(cart, selectedCoupon);
+    return {
+      totalBeforeDiscount: Math.round(totalBeforeDiscount),
+      totalAfterDiscount: Math.round(totalAfterDiscount),
+      totalDiscount: Math.round(totalDiscount),
+    };
+  };
 
   return {
     cart,
-    addToCart,
-    removeFromCart,
-    updateQuantity,
-    applyCoupon,
-    calculateTotal,
+    addToCart: addToCart,
+    removeFromCart: removeFromCart,
+    updateQuantity: updateQuantity,
+    applyCoupon: applyCoupon,
+    calculateTotal: calculateTotal,
     selectedCoupon,
   };
 };
