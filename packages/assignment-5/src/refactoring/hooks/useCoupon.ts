8fb6bb0394a8c80778ed2feb06d18811c32d1cb6
Rachel <borapark13@gmail.com>
2024-07-18 15:38:22 +0900

    feat: useCoupon 커스텀 훅 추가

--- packages/assignment-5/src/refactoring/hooks/useCoupon.ts
+++ packages/assignment-5/src/refactoring/hooks/useCoupon.ts
@@ -1,6 +1,20 @@
-import { Coupon } from '../../types.ts';
 import { useState } from 'react';
 
+import { Coupon } from '../../types.ts';
+
 export const useCoupons = (initialCoupons: Coupon[]) => {
-  return { coupons: [], addCoupon: () => undefined };
+  const [coupons, setCoupons] = useState<Coupon[]>(initialCoupons);
+
+  const addCoupon = (newCoupon: Coupon) => {
+    setCoupons((prevCoupon) => [...prevCoupon, newCoupon]);
+  };
+  const removeCoupon = (couponCode: string) => {
+    setCoupons((prevCoupon) => prevCoupon.filter((coupon) => coupon.code !== couponCode));
+  };
+  const updateCoupon = (updatedCoupon: Coupon) => {
+    setCoupons((prevCoupon) =>
+      prevCoupon.map((coupon) => (coupon.code === updatedCoupon.code ? updatedCoupon : coupon))
+    );
+  };
+  return { coupons, addCoupon, removeCoupon, updateCoupon };
 };
