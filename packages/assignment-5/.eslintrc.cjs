d77fa0722ad56b0449a546ef1e51e2a6b405c108
ChangigJeon <wjs1724@naver.com>
2024-07-18 23:42:39 +0900

    fix 설정값 변경

--- packages/assignment-5/.eslintrc.cjs
+++ packages/assignment-5/.eslintrc.cjs
@@ -5,14 +5,25 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    curly: ['error'],
+    eqeqeq: ['error', 'always'],
+    'no-unused-vars': ['error', { vars: 'all', args: 'none' }],
+    'no-var': ['error'],
+    'no-trailing-spaces': ['error'],
+    'object-curly-spacing': ['error', 'always'],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
-}
+};
