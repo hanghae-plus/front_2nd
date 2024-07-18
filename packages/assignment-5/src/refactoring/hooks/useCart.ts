45a426e346d60913dbd143d08c073543ae5994a2
Rachel <borapark13@gmail.com>
2024-07-18 17:13:24 +0900

    fix: 이미 존재하는 상품이 카트에 추가되는 경우 추가

--- packages/assignment-5/src/refactoring/hooks/useCart.ts
+++ packages/assignment-5/src/refactoring/hooks/useCart.ts
@@ -9,7 +9,24 @@ export const useCart = () => {
   const [selectedCoupon, setSelectedCoupon] = useState<Coupon | null>(null);
 
   const addToCart = (product: Product) => {
-    setCart((prevCart) => [...prevCart, { product, quantity: 1 }]);
+    // 이미 존재하는 상품의 경우
+    const exitedItem = cart.find((item) => item.product.id === product.id);
+    if (exitedItem) {
+      setCart((prevCart) => {
+        return prevCart.map((item) => {
+          if (item.product.id === product.id) {
+            return {
+              ...item,
+              quantity: item.quantity + 1
+            };
+          }
+          return item;
+        });
+      });
+    } else {
+      // 새로운 상품의 경우
+      setCart((prevCart) => [...prevCart, { product, quantity: 1 }]);
+    }
   };
 
   const removeFromCart = (productId: string) => {
