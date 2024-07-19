d916520343b7af46c02ac89a4e292b2439be00c0
Raihyul <tlompy1104@gmail.com>
2024-07-19 09:44:18 +0900

    [basic-2] refact: calculateCart 수정
    - let 선언부 const로 변경
    - if-else문 삼항연산자로 변경

--- packages/assignment-5/src/refactoring/helpers/calculateCart.ts
+++ packages/assignment-5/src/refactoring/helpers/calculateCart.ts
@@ -11,16 +11,12 @@ export const getMaxApplicableDiscount = (item: CartItem) => {
 };
 
 export const calculateItemTotal = (item: CartItem) => {
-  let totalBeforeDiscount = 0;
-  let totalAfterDiscount = 0;
-
   const { price } = item.product;
   const { quantity } = item;
-  totalBeforeDiscount += price * quantity;
+  const totalBeforeDiscount = price * quantity;
 
   const discount = getMaxApplicableDiscount(item);
-
-  totalAfterDiscount += totalBeforeDiscount * (1 - discount);
+  const totalAfterDiscount = totalBeforeDiscount * (1 - discount);
 
   return totalAfterDiscount;
 };
@@ -41,11 +37,10 @@ export const calculateCartTotal = (cart: CartItem[], selectedCoupon: Coupon | nu
 
   // 쿠폰 적용
   if (selectedCoupon) {
-    if (selectedCoupon.discountType === 'amount') {
-      totalAfterDiscount = Math.max(0, totalAfterDiscount - selectedCoupon.discountValue);
-    } else {
-      totalAfterDiscount *= 1 - selectedCoupon.discountValue / 100;
-    }
+    selectedCoupon.discountType === 'amount'
+      ? (totalAfterDiscount = Math.max(0, totalAfterDiscount - selectedCoupon.discountValue))
+      : (totalAfterDiscount *= 1 - selectedCoupon.discountValue / 100);
+
     totalDiscount = totalBeforeDiscount - totalAfterDiscount;
   }
 
