ec314da92eb40900cf6ab7cff49d1d66bc82deaa
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2024-07-22 18:13:27 +0900

    chore : lighthouse setting

--- packages/assignment-6/.lighthouserc.cjs
+++ packages/assignment-6/.lighthouserc.cjs
@@ -1,8 +1,8 @@
 module.exports = {
   ci: {
     collect: {
-      startServerCommand: "pnpm run build", // 서버를 키는 명령어를 통해서도 실행 가능
-      url: ["http://localhost:3000"],
+      startServerCommand: "pnpm run start", // 서버를 키는 명령어를 통해서도 실행 가능
+      url: ["http://localhost:5173"],
       numberOfRuns: 3,
     },
     upload: {
