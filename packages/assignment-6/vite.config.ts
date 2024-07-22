f32ed57aadd8191150035ded3288764f4fb6e7a6
seojunhwan <seojunhwan@kakao.com>
2024-07-22 22:41:08 +0900

    chore: test coverage 추가

--- packages/assignment-6/vite.config.ts
+++ packages/assignment-6/vite.config.ts
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react-swc';
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    test:{
+      coverage:{
+        provider:'v8'
+      }
+    }
   }),
   defineTestConfig({
     test: {
