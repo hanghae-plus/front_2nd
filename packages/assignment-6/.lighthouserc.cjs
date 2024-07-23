090b2c9cfbf6e8b9afe9f3206a8b3f64c39e48ee
soojjung <89066676+soojjung@users.noreply.github.com>
2024-07-24 03:44:57 +0900

    fix: ci lighthouse

--- packages/assignment-6/.lighthouserc.cjs
+++ packages/assignment-6/.lighthouserc.cjs
@@ -1,21 +1,13 @@
 module.exports = {
   ci: {
     collect: {
-      startServerCommand: "pnpm -F assignment-6 start",
+      staticDistDir: "./build",
+      // startServerCommand: "pnpm -F assignment-6 start", // 서버를 키는 명령어를 통해서도 실행 가능
       url: ["http://localhost:5173"],
       numberOfRuns: 5,
     },
     upload: {
-      target: "filesystem",
-      outputDir: "./lhci-results",
-    },
-    assert: {
-      assertions: {
-        "categories:performance": ["warn", { minScore: 0.9 }],
-        "categories:accessibility": ["warn", { minScore: 0.9 }],
-        "categories:best-practices": ["warn", { minScore: 0.9 }],
-        "categories:seo": ["warn", { minScore: 0.9 }],
-      },
+      target: "temporary-public-storage",
     },
   },
 };
