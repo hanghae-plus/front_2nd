64206ecbc955e615439fec2c3763c55d6c648db2
haeji1124 <46364329+haeji1124@users.noreply.github.com>
2024-07-24 05:35:56 +0900

    .

--- packages/assignment-6/.lighthouserc.cjs
+++ packages/assignment-6/.lighthouserc.cjs
@@ -1,17 +1,12 @@
 module.exports = {
-    ci: {
-      collect: {
-        startServerCommand: "pnpm -F assignment-6 start",
-        url: ["http://localhost:5173/"],
-        numOfRuns: 5,
-        settings: {
-          preset: "desktop",
-        },
-      },
-      upload: {
-        target: "filesystem",
-        outputDir: "./lhci_reports",
-        reportFilenamePattern: "%%PATHNAME%%-%%DATETIME%%-report.%%EXTENSION%%",
-      },
+  ci: {
+    collect: {
+      startServerCommand: "pnpm -F assignment-6 start",
+      url: ["http://localhost:5173/"]
     },
-  };
+    upload: {
+      target: "filesystem",
+      outputDir: "./lhci_reports",
+    },
+  }
+}
