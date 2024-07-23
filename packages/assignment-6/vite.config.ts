e6134b834c6be062b31f702b667ddd90d60beda8
2pandi <coder.2pandi@gmail.com>
2024-07-23 21:50:16 +0900

    update

--- packages/assignment-6/vite.config.ts
+++ packages/assignment-6/vite.config.ts
@@ -1,16 +1,28 @@
-import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
-import { defineConfig } from 'vite';
+import { defineConfig, UserConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import {
+  defineConfig as defineTestConfig,
+  mergeConfig,
+  UserConfig as VitestUserConfig,
+} from 'vitest/config';
+
+const viteConfig: UserConfig = {
+  plugins: [react()],
+};
+
+const vitestConfig: VitestUserConfig = {
+  test: {
+    coverage: {
+      provider: 'istanbul',
+      reporter: ['text', 'json', 'html'],
+    },
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+  },
+};
 
 export default mergeConfig(
-  defineConfig({
-    plugins: [react()],
-  }),
-  defineTestConfig({
-    test: {
-      globals: true,
-      environment: 'jsdom',
-      setupFiles: './src/setupTests.ts'
-    },
-  })
-)
+  defineConfig(viteConfig),
+  defineTestConfig(vitestConfig)
+);
