4d0eba6042839e491d181d0b638921449032b09a
soojjung <89066676+soojjung@users.noreply.github.com>
2024-07-24 03:47:05 +0900

    Update .lighthouserc.cjs

--- packages/assignment-6/.lighthouserc.cjs
+++ packages/assignment-6/.lighthouserc.cjs
@@ -1,8 +1,7 @@
 module.exports = {
   ci: {
     collect: {
-      staticDistDir: "./build",
-      // startServerCommand: "pnpm -F assignment-6 start", // 서버를 키는 명령어를 통해서도 실행 가능
+      startServerCommand: "pnpm -F assignment-6 start", // 서버를 키는 명령어를 통해서도 실행 가능
       url: ["http://localhost:5173"],
       numberOfRuns: 5,
     },
