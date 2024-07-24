8b483ecddaf538e9766c2f9612b2c0c14c50a3db
hiimhyewon <HWUHLyVper5mDbGxBqXPyM3omwE1DOT+Z/UNy8izQ70=>
2024-07-24 11:31:05 +0900

    [추가] lighthouserc 파일 및 .env 파일 추가

---
+++ packages/assignment-6/.lighthouserc.cjs
@@ -0,0 +1,19 @@
+require('dotenv').config()
+
+module.exports = {
+    ci: {
+        collect: {
+            startServerCommand: "pnpm -F assignment-6 start",
+            url: [process.env.VITE_HOST+'/'],
+            numberOfRuns: 5,
+            settings: {
+                preset: "desktop",
+            },
+        },
+        upload: {
+            target: "filesystem",
+            outputDir: "./lhci_reports",
+            reportFilenamePattern: "%%PATHNAME%%-%%DATETIME%%-report.%%EXTENSION%%",
+        },
+    },
+};
