e30040d27f360e5d20ff8a00b5fee7023f53b4c6
HYEWON <hiimhyewon@naver.com>
2024-07-24 22:09:04 +0900

    [수정] lhci env 경로 제거

--- packages/assignment-6/.lighthouserc.cjs
+++ packages/assignment-6/.lighthouserc.cjs
@@ -1,10 +1,11 @@
-require('dotenv').config()
+// require('dotenv').config()
 
 module.exports = {
     ci: {
         collect: {
             startServerCommand: "pnpm -F assignment-6 start",
-            url: [process.env.VITE_HOST],
+            // url: [process.env.VITE_HOST],
+            url: ["http://localhost:5173/"],
             numOfRuns: 5,
             settings: {
                 preset: "desktop",
