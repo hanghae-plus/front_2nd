7313185a3906b9e416cd942262abcb34f78148ff
hiimhyewon <HWUHLyVper5mDbGxBqXPyM3omwE1DOT+Z/UNy8izQ70=>
2024-07-24 12:31:20 +0900

    [수정] lighthouserc 수정

--- packages/assignment-6/.lighthouserc.cjs
+++ packages/assignment-6/.lighthouserc.cjs
@@ -3,9 +3,8 @@ require('dotenv').config()
 module.exports = {
     ci: {
         collect: {
-            staticDistDir: "./packages/assignment-6/dist",
             startServerCommand: "pnpm -F assignment-6 start",
-            url: [process.env.VITE_HOST+'/'],
+            url: [process.env.VITE_HOST],
             numberOfRuns: 5,
             settings: {
                 preset: "desktop",
