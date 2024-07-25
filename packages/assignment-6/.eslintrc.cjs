ec90ea5ffe101741a453023de7cfcfdc1295b8fc
ChangigJeon <wjs1724@naver.com>
2024-07-26 00:15:04 +0900

    fix 2

--- packages/assignment-6/.eslintrc.cjs
+++ packages/assignment-6/.eslintrc.cjs
@@ -1,14 +1,26 @@
-module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-  ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh", "@typescript-eslint"],
-  rules: {
-    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+import js from "@eslint/js";
+import ts from "@typescript-eslint/eslint-plugin";
+import parser from "@typescript-eslint/parser";
+import reactRefresh from "eslint-plugin-react-refresh";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": ts,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      // 다른 규칙 추가 가능
+    },
   },
-};
+];
