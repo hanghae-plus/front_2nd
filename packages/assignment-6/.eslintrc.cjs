d8e2a214f0022eb23e546922a66d149612f9b167
ChangigJeon <wjs1724@naver.com>
2024-07-25 23:15:44 +0900

    fix 린트 수정

--- packages/assignment-6/.eslintrc.cjs
+++ packages/assignment-6/.eslintrc.cjs
@@ -12,12 +12,4 @@ module.exports = {
   rules: {
     "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
   },
-  overrides: [
-    {
-      files: ["**/*.ts", "**/*.tsx"],
-      rules: {
-        // 추가적인 TypeScript 관련 규칙들을 여기에 작성합니다.
-      },
-    },
-  ],
 };
