e51d45241af90923e9e8f3677f4ddd4ff6624cba
metacode22 <alohajune22@gmail.com>
2024-07-25 22:46:40 +0900

    vitest coverage 명령어 수정

--- packages/assignment-6/vite.config.ts
+++ packages/assignment-6/vite.config.ts
@@ -5,18 +5,16 @@ import react from "@vitejs/plugin-react-swc";
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
-    test: {
-      coverage: {
-        reporter: ["text", "json-summary", "json"],
-        reportOnFailure: true,
-      },
-    },
   }),
   defineTestConfig({
     test: {
       globals: true,
       environment: "jsdom",
       setupFiles: "./src/setupTests.ts",
+      coverage: {
+        reporter: ["text", "json-summary", "json"],
+        reportOnFailure: true,
+      },
     },
   })
 );
