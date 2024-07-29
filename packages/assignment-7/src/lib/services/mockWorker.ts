5c178bd13ddd3c3adf5b2b5a83302725d035e901
LucetTin5 <Lucettin5@gmail.com>
2024-07-29 21:48:39 +0900

    fix: msw/browser 제거

--- packages/assignment-7/src/lib/services/mockWorker.ts
+++ packages/assignment-7/src/lib/services/mockWorker.ts
@@ -1,9 +1,4 @@
-import { setupWorker } from 'msw/browser';
 import { setupServer } from 'msw/node';
 import { handlers } from './mockHandlers';
 
-// Integration, Unit Test
 export const mockServer = setupServer(...handlers);
-
-// E2E Test
-export const mockWorker = setupWorker(...handlers);
