6fb66ef91ae2955481a7cf0610901f5a7c53ce88
seojunhwan <seojunhwan@kakao.com>
2024-07-31 23:59:21 +0900

    feat: setup msw

--- packages/assignment-7/src/main.tsx
+++ packages/assignment-7/src/main.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import { ChakraProvider } from '@chakra-ui/react'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { ChakraProvider } from '@chakra-ui/react';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>,
-)
+async function prepare() {
+  const { setupWorker } = await import('msw/browser');
+  const { mockApiHandlers } = await import('./mocks/handlers');
+  const worker = setupWorker(...mockApiHandlers);
+  await worker.start();
+}
+
+prepare().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </React.StrictMode>
+  );
+});
