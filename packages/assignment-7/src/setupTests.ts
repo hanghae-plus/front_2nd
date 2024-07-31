5ac027c8dfc958c7acd53e8697c8d3446cf5b5b0
yoosion030 <yoosioff@gmail.com>
2024-07-31 23:11:00 +0900

    7주차 과제 제출 - basic
    Signed-off-by: yoosion030 <yoosioff@gmail.com>

--- packages/assignment-7/src/setupTests.ts
+++ packages/assignment-7/src/setupTests.ts
@@ -1,1 +1,20 @@
-import '@testing-library/jest-dom';
+import { setupServer } from "msw/node";
+import "@testing-library/jest-dom";
+import { afterAll, afterEach, beforeAll, vi } from "vitest";
+import { mockApiHandlers } from "./mockApiHandlers";
+
+const server = setupServer(...mockApiHandlers);
+
+beforeAll(() => {
+  server.listen();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+  vi.clearAllMocks();
+});
+
+afterAll(() => {
+  vi.resetAllMocks();
+  server.close();
+});
---
+++ packages/assignment-7/src/utils/date.ts
@@ -0,0 +1,33 @@
+export const getDaysInMonth = (year: number, month: number) => {
+  return new Date(year, month + 1, 0).getDate();
+};
+
+export const getWeekDates = (date: Date) => {
+  const day = date.getDay();
+  const diff = date.getDate() - day;
+
+  const sunday = new Date(date.getFullYear(), date.getMonth(), diff);
+
+  const weekDates = [];
+  for (let i = 0; i < 7; i++) {
+    const nextDate = new Date(sunday);
+    nextDate.setDate(sunday.getDate() + i);
+    weekDates.push(nextDate);
+  }
+  return weekDates;
+};
+
+export const formatWeek = (date: Date): string => {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const weekNumber = Math.ceil(date.getDate() / 7);
+  return `${year}년 ${month}월 ${weekNumber}주`;
+};
+
+export const formatMonth = (date: Date): string => {
+  if (!date.getFullYear() || date.getMonth() < 0) return "";
+
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  return `${year}년 ${month}월`;
+};
