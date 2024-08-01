5387b4b4d50a2160fadba4c9a5750620d6d0513a
LucetTin5 <Lucettin5@gmail.com>
2024-08-01 23:42:43 +0900

    remove: unused file

--- packages/assignment-7/src/constants/api.ts
+++
@@ -1,7 +0,0 @@
-export const API_MAP = {
-  events: '/api/events',
-  singleEvent: (id: string) => `/api/events/${id}`,
-};
-
-export const HOLIDAYS_API =
-  'http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService';
