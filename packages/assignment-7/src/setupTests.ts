d8a463523362a8f4368fcbde3ef799c94a65d43d
seojunhwan <seojunhwan@kakao.com>
2024-08-01 22:43:36 +0900

    chore: setupTest

--- packages/assignment-7/src/setupTests.ts
+++ packages/assignment-7/src/setupTests.ts
@@ -1,1 +1,22 @@
 import '@testing-library/jest-dom';
+import { setupServer } from 'msw/node';
+import { afterEach, beforeEach, vi } from 'vitest';
+import { mockApiHandlers } from './mocks/handlers';
+
+const server = setupServer(...mockApiHandlers);
+
+beforeEach(() => {
+  vi.useFakeTimers({
+    shouldAdvanceTime: true,
+  });
+
+  vi.setSystemTime(new Date('2024-08-01'));
+});
+
+afterEach(() => {
+  server.resetHandlers();
+  vi.clearAllMocks();
+
+  vi.runOnlyPendingTimers();
+  vi.useRealTimers();
+});
