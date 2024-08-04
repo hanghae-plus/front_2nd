dddfe9aea3a049a6de4471410fa872e98b9133b4
surffhy <4538asd@gmail.com>
2024-08-04 10:26:29 +0900

    Revert "7주차 과제 제출 - 김혜성"

--- packages/assignment-7/src/main.tsx
+++ packages/assignment-7/src/main.tsx
@@ -1,24 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import { ChakraProvider } from "@chakra-ui/react";
-import { EventProvider } from "./hooks/useEvents.tsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import { ChakraProvider } from '@chakra-ui/react'
 
-async function prepare() {
-  const { setupWorker } = await import("msw/browser");
-  const { mockApiHandlers } = await import("./mockApiHandlers.ts");
-  const worker = setupWorker(...mockApiHandlers);
-  return worker.start();
-}
-
-prepare().then(() => {
-  ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-      <EventProvider>
-        <ChakraProvider>
-          <App />
-        </ChakraProvider>
-      </EventProvider>
-    </React.StrictMode>
-  );
-});
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>,
+)
--- packages/assignment-7/src/utils/formatMonth.ts
+++
@@ -1,5 +0,0 @@
-export const formatMonth = (date: Date): string => {
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  return `${year}년 ${month}월`;
-};
