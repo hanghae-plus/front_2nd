5a512066833788b73ad8c25d788ee4a96c029459
Joie-Kim <joie.huiju@gmail.com>
2024-08-05 23:49:12 +0900

    [8주차] 기본과제 - 테스트 시나리오 작성

---
+++ packages/assignment-8/src/__tests__/recurringEvent.test.tsx
@@ -0,0 +1,31 @@
+// INFO: 8주차 과제 파일
+
+describe('반복 일정 기능', () => {
+  describe('새 일정 생성', () => {
+    test.fails('반복 설정을 할 수 있다', async () => {});
+    test.fails('반복 간격을 설정할 수 있다', async () => {});
+  });
+
+  describe('반복 일정 표시', () => {
+    test.fails('캘린더 뷰에 반복 일정이 올바르게 표시된다', async () => {});
+  });
+
+  describe('반복 일정 수정', () => {
+    test.fails('특정 날짜의 일정만 수정할 수 있다', async () => {});
+    test.fails('특정 날짜 이후의 모든 일정을 수정할 수 있다', async () => {});
+  });
+
+  describe('반복 일정 삭제', () => {
+    test.fails('특정 날짜의 일정만 삭제할 수 있다', async () => {});
+    test.fails('특정 날짜 이후의 모든 일정을 삭제할 수 있다', async () => {});
+    test.fails('전체 반복 일정을 삭제할 수 있다', async () => {});
+  });
+
+  describe('반복 규칙 수정', () => {
+    test.fails('반복 일정의 반복 규칙을 수정할 수 있다', async () => {});
+  });
+
+  describe('예외 날짜 처리', () => {
+    test.fails('반복 일정에 예외 날짜를 추가할 수 있다', async () => {});
+  });
+});
